f64c66c322b8879b8435d0f3b58bbb5b81837899
Nick Andrew <nick@nick-andrew.net>
2002-05-31 12:04:07 +1000

    Initial Commit 14-Feb-88

---
+++ zeta-bbs/treeboard/bb/bb.asm
@@ -0,0 +1,40 @@
+;BB: The main part of the Treeboard
+*GET	DOSCALLS.HDR
+*GET	EXTERNAL.HDR
+*GET	ASCII.HDR
+;
+MAX_LINES	EQU	250
+MAX_MSGS	EQU	1024
+;
+	ORG	PROG_START
+	DEFW	BASE
+	DEFW	THIS_PROG_END
+	DEFW	0
+	DEFW	CLEAN_DISCON
+;End of program load info.
+;
+	COM	'<BB 1.7b 14-Feb-88>'
+;
+	ORG	BASE+100H
+;
+*GET	BB1		;Code 1
+*GET	BB2		;Code 2
+*GET	BB3		;Code 3
+*GET	BB4		;Message entry
+*GET	BB5		;Subroutines
+*GET	BB6		;Resend command
+*GET	BB7		;Text file routines
+;
+*GET	LINEIN.LIB
+*GET	TIMES.LIB
+*GET	ROUTINES.LIB
+;
+*GET	BBDATA
+;
+ZZZZZZZY EQU $		;End of required data
+	DEFS	4096	;Text buffer.
+ZZZZZZZZ EQU $		;End of 4k buffer.
+;
+THIS_PROG_END	EQU	$
+;
+	END	START
