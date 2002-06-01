94b9ee968632272b236e040a7e42988d631186b6
Nick Andrew <nick@nick-andrew.net>
2002-06-02 01:19:03 +1000

    Longer version, I don't understand the difference

--- zeta-bbs/maintain/credit/credit.asm
+++ zeta-bbs/maintain/credit/credit.asm
@@ -1,46 +1,110 @@
 ;credit: Assemble a CREDIT, compiled C, for Zeta
 ;
-*GET	DOSCALLS
-*GET	EXTERNAL
-*GET	ASCII
+ZETA		EQU	1
+DEBUGF		EQU	0	;Function call debugging
+DEBUGG		EQU	0	;Loop at start debug
+SYSOPONLY	EQU	1	;Only the Sysop may run
+REDIRDIS	EQU	1	;1 to disable redirection
+STACKSIZE	EQU	100H	;Size of the stack
 ;
+*GET	DOSCALLS:0
+;
+	IF	ZETA
+*GET	EXTERNAL.HDR
+*GET	ASCII.HDR
+;
+	COM	'<credit 1.0  14-Aug-87>'
 	ORG	PROG_START
 	DEFW	BASE
 	DEFW	THIS_PROG_END
 	DEFW	0
 	DEFW	0
-;End of program load info
+;End of program load info.
+	ORG	BASE+STACKSIZE
+	ELSE
+	ORG	5200H+STACKSIZE
 ;
-	COM	'<credit 1.0  14-Aug-87>'
-	ORG	BASE+100H
+	ENDIF
+;
+TOPSTACK	EQU	$
+;
+	IF	DEBUGF
+*GET	DEBUGF
+	ELSE
+DEBUG	MACRO	#$STR
+	ENDM
+	ENDIF
+;
 START
+	IF	ZETA
 START1	DEC	HL
 	LD	A,(HL)
 	CP	' '
 	JR	NC,START1
 	INC	HL		;Pseudo start of cmd line
-	LD	SP,START	;There is plenty of stack
-	LD	(CMDLINE),HL	;Save cmd line pointer
+;
+	LD	SP,TOPSTACK	;There is plenty of stack
+	LD	(_CMDLINE),HL	;Save cmd line pointer
+	LD	HL,REDIRDIS	;Disable redirection
+	LD	(_NOREDIR),HL
+;
+	IF	SYSOPONLY
 	LD	A,(PRIV_1)
 	BIT	IS_SYSOP,A
 	LD	A,0
 	JP	Z,TERMINATE
+	ENDIF
+;
+	ELSE
+;
+	LD	HL,(HIMEM)
+	LD	SP,HL
+;
+	ENDIF
+;
+	IF	DEBUGG
+DB_LOOP
+	JP	DB_LOOP
+	ENDIF
 ;
 *GET	CINIT
 *GET	CALL
 ;
-DEBUG	MACRO	#$S
-	ENDM
-;
 *GET	CREDIT1
 *GET	CREDIT2
 ;
-*GET	ROUTINES
+;;*GET	ARCSUBS
+*GET	ATOI		;Requires ctype
+*GET	CTYPE		;Required by atoi
+;;*GET	FTELL
+;;*GET	FWRITE
+;;*GET	GETOPT
+;;*GET	GETTIME
+;;*GET	GETW
+;;*GET	INDEX
+;;*GET	PNUMB
+;;*GET	SAVEPOS
+;;*GET	STRCHR
+;;*GET	STRCMP
+;;*GET	STRLEN
+;;*GET	SYSTEM
+;;*GET	UNLINK
+;;*GET	WILD
+;
+	IF	ZETA
+*GET	ROUTINES.LIB
+*GET	LIBCZ
+	ELSE
 *GET	LIBC
-*GET	ATOI
-*GET	CTYPE
+	ENDIF
 ;
-HIGHEST	DEFW	$+2
+_CMDLINE	DEFW	4318H
+_NOREDIR	DEFW	0
+_BRKSIZE	DEFW	$+2
 ;
+	IF	ZETA
 THIS_PROG_END	EQU	$
+	ENDIF
+;
 	END	START
+
