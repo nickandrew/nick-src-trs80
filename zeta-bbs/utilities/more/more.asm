a4333f7072c98e8100b0fed1b6c9c468fafe674a
Nick Andrew <nick@nick-andrew.net>
2002-06-02 13:14:56 +1000

    *** empty log message ***

---
+++ zeta-bbs/utilities/more/more.asm
@@ -0,0 +1,78 @@
+;more: Assemble more, file paginator
+;
+ZETA	EQU	1
+;
+*GET	DOSCALLS:0
+;
+	IF	ZETA
+*GET	EXTERNAL
+*GET	ASCII
+;
+	COM	'<More 1.1b 18-Jan-88>'
+	ORG	PROG_START
+	DEFW	BASE
+	DEFW	THIS_PROG_END
+	DEFW	0
+	DEFW	TERMINATE
+;End of program load info.
+	ORG	BASE+100H
+	ELSE
+	ORG	5200H
+;
+	ENDIF
+;
+;;*GET	DEBUG:1
+DEBUG	MACRO	#$STR
+	ENDM
+;
+START
+	IF	ZETA
+START1	DEC	HL
+	LD	A,(HL)
+	CP	' '
+	JR	NC,START1
+	INC	HL		;Pseudo start of cmd line
+;
+	LD	SP,START	;There is plenty of stack
+	LD	(_CMDLINE),HL	;Save cmd line pointer
+	LD	HL,1		;Disable redirection
+	LD	(_NOREDIR),HL
+;
+;;	LD	A,(PRIV_1)
+;;	BIT	IS_SYSOP,A
+;;	LD	A,0
+;;	JP	Z,TERMINATE
+;
+	ELSE
+;
+	LD	HL,(HIMEM)
+	LD	SP,HL
+;
+	ENDIF
+;
+*GET	CINIT:1
+*GET	CALL:1
+;
+*GET	MORE1
+;
+*GET	INDEX
+*GET	STRCMP
+*GET	STRLEN
+;
+	IF	ZETA
+*GET	ROUTINES
+*GET	LIBCZ
+	ELSE
+*GET	LIBC:1
+	ENDIF
+;
+_CMDLINE	DEFW	4318H
+_NOREDIR	DEFW	0
+;
+HIGHEST	DEFW	$+2
+;
+	IF	ZETA
+THIS_PROG_END	EQU	$
+	ENDIF
+;
+	END	START
