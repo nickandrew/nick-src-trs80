dc62923107f1274343fcce26eb051d4d98ee54e9
Nick Andrew <nick@nick-andrew.net>
2002-06-02 11:36:25 +1000

    1988-04-03

---
+++ zeta-bbs/internal/cron/cron.asm
@@ -0,0 +1,86 @@
+;cron: Assemble a CRON, compiled C, for Zeta
+;
+ZETA	EQU	1
+;
+*GET	DOSCALLS.HDR
+;
+	IF	ZETA
+*GET	EXTERNAL.HDR
+*GET	ASCII.HDR
+;
+	COM	'<Cron 1.0c 03-Apr-88>'
+	ORG	PROG_START
+	DEFW	BASE
+	DEFW	THIS_PROG_END
+	DEFW	0
+	DEFW	0
+;End of program load info.
+	ORG	BASE+100H
+	ELSE
+	ORG	5200H
+;
+	ENDIF
+;
+;;*GET	DEBUG:1
+DEBUG	MACRO	#$STR
+	ENDM
+;
+START
+	IF	ZETA
+START1	DEC	HL
+	LD	A,(HL)
+	CP	' '
+	JR	NC,START1
+	INC	HL		;Pseudo start of cmd line
+;
+	LD	SP,START	;There is plenty of stack
+	LD	(_CMDLINE),HL	;Save cmd line pointer
+	LD	HL,1		;Disable redirection
+	LD	(_NOREDIR),HL
+;
+	LD	A,(PRIV_1)
+	BIT	IS_SYSOP,A
+	LD	A,0
+	JP	Z,TERMINATE
+;
+	ELSE
+;
+	LD	HL,(HIMEM)
+	LD	SP,HL
+;
+	ENDIF
+;
+*GET	CINIT
+*GET	CALL
+;
+*GET	CRON1
+*GET	CRON2
+;
+;;*GET	ATOI
+;;*GET	CTYPE
+;;*GET	FTELL
+;;*GET	FWRITE
+;;*GET	INDEX
+;;*GET	STRCMP
+;;*GET	STRLEN
+;;*GET	SYSTEM
+;;*GET	UNLINK
+;;*GET	WILD
+;
+	IF	ZETA
+*GET	ROUTINES.LIB
+*GET	LIBCZ
+	ELSE
+*GET	LIBC
+	ENDIF
+;
+_CMDLINE	DEFW	4318H
+_NOREDIR	DEFW	0
+;
+HIGHEST	DEFW	$+2
+;
+	IF	ZETA
+THIS_PROG_END	EQU	$
+	ENDIF
+;
+	END	START
