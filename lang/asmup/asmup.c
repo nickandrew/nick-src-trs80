6e72382e85b203d4b43a8660bd9917ced6e32536
Nick Andrew <nick@nick-andrew.net>
2002-06-04 13:55:25 +1000

    convert assembler source to uppercase

---
+++ lang/asmup/asmup.c
@@ -0,0 +1,32 @@
+/* ASMUP - convert assembler source to uppercase */
+/* Writes to stdout, ignores comments (ie: ";")  */
+/* Handles lines longer than 80 chars            */
+
+#include <stdio.h>
+#include <ctype.h>
+
+char buf[81],*c,q;
+
+main()
+{
+    while (fgets(buf, 80, stdin)!=NULL) {
+        c=buf;
+        while (*c) {
+
+            /* ignore the innards of strings etc */
+            if (*c=='\'' || *c=='"') {
+                q= *c;
+                while (*++c!=q && *c);
+                c=(q ? ++c : c);
+                continue;
+            }
+
+            /* bypass conversion if comment */
+            if (*c==';') break;
+
+            if (*c>='a' && *c<='z') *c=toupper(*c);
+            ++c;
+        }
+        fputs(buf,stdout);
+    }
+}
