fe27de3e154e9fa4e2ffa99c5d9125d28ada3001
Nick Andrew <nick@nick-andrew.net>
2002-06-07 23:11:41 +1000

    Updated 1985-11-11

--- utilities/report-writer/ppk1.c
+++ utilities/report-writer/ppk1.c
@@ -2,51 +2,47 @@
  * PPK Ass #1 - Nick Andrew
  * Student # 8425464
  * 03-Sep-85
+ * 11-Nov-85: Modified for Alcor 'C'
  */
 
-#define SORTIN "temp1.f"             /* Temporary files       */
-#define SORTOUT "temp2.f"
-#define MAXFLDS 100                  /* Maximum No. of fields */
-#include stdio/csh                   /* Get standard routines */
+#define SORTIN "temp1/f"             
+#define SORTOUT "temp2/f"
+#define MAXFLDS (100)           /* Maximum No. of fields */
+
+#include <stdio.h>              /* Get standard routines */
+
 FILE *config, *src, *dest, *temp1, *temp2;
 char *heading,*today,*usecr;
+
 int sortdat[MAXFLDS],printdat[MAXFLDS],mwidth[MAXFLDS];
 int numflds,numsort,numprint,lines,sortlen,page;
 int sumwidth,columns,spacing,linpage;
- /* An array of records */
-          char *tname[MAXFLDS],*dataptr[MAXFLDS]
-               ,*display[MAXFLDS];
-          int  flength[MAXFLDS];
-          char needed[MAXFLDS],prneed[MAXFLDS]
-               ,srneed[MAXFLDS],delim[MAXFLDS],numer[MAXFLDS];
-          char fieldat[MAXFLDS];
 
-/* the main program function and initial entry point */
+struct       {
+               char *tname,*dataptr,*display;
+               int  flength;
+               char needed,prneed,srneed,delim,numer;
+             } fieldat[MAXFLDS];
+
+
 main(argc,argv)
 int argc; char *argv[];
 {
 
-   intro();                              /* print introduction and get time */
-   getconf(argv[1],(argc>1));            /* get configuration data */
-   copy();                               /* Copy source file to temp1 */
-   dosort();                             /* Sort the temporary file */
-   dorprt();                             /* print a report */
+   intro();                              
+   getconf(argv[1],(argc>1));            
+   copy();                               
+   dosort();                             
+   dorprt();                             
    printf("PPK1: Normal Termination\n");
 }
 
-/* Print a short introduction to the program
- * Get a date string off the system
- */
 intro()
 {
-   long now;
-   char *ctime();
    printf("PPK Assignment 1 - Nick Andrew\n");
-   time(&now);
-   today=ctime(&now);
 }
 
-/* Get intut file configuration and output format data
+/* Get input file configuration and output format data
  * off either standard input (keyboard) or from a file.
  */
 getconf(confil,flag)
@@ -67,10 +63,10 @@ int  flag;
    fscanf(config,"%s",string);
    dest=openup(string,"w");
    /* check for correct config file */
-   if (flag && (comma(fscanf(config," %c",&c),c!='*')))
+   if (flag && (fscanf(config," %c",&c),c!='*'))
       printf("PPK1: Error in config file!\n");
    prompt(flag,"Enter a heading for this report: ");
-   heading=getstring(config);                    /* get heading string */
+   heading=getstring(config);       /* get heading string */
    printf("Heading is %s\n",heading);
    prompt(flag,"Use how many columns? (80/132): ");
    fscanf(config,"%d",&columns);
@@ -94,27 +90,27 @@ int  flag;
       fscanf(config,"%s",string);
       if (string[0]=='*') break; /* break out of the loop */
 
-      tname[++numflds]=calloc(strlen(string)+1,sizeof(char));
-      strcpy(tname[numflds],string);
-      needed[numflds]=0;
+      fieldat[++numflds].tname=calloc(strlen(string)+1,sizeof(char));
+      strcpy(fieldat[numflds].tname,string);
+      fieldat[numflds].needed=0;
                      /* read the integer field length */
       prompt(flag,"Enter maximum field length: ");
-      fscanf(config,"%d",&flength[numflds]);
+      fscanf(config,"%d",&fieldat[numflds].flength);
       prompt(flag,"Enter <A> Ascii or <N> Numeric field: ");
       fscanf(config," %c",&c);
       c=toupper(c);
-      numer[numfields]=(c=='N');
+      fieldat[numflds].numer=(c=='N');
       prompt(flag,"Enter delimiter char or CR if none: ");
       if (!flag) do c=getc(config); while (c!='\n');
                             /* bypass white space */
       do c=getc(config); while (c==' ');
       if (c=='\n') c=0;     /* no delimiter */
       if (c==0x5c)          /* read an octal number */
-         { short oct;
+         { int oct;
            fscanf(config,"%3o",&oct);
-           c=oct;
+           c=(char) oct;
          }
-      delim[numflds]=c;
+      fieldat[numflds].delim=c;
       }
 
    numsort=numprint=0;
@@ -125,14 +121,14 @@ int  flag;
       fscanf(config,"%s",string);
       if (string[0]=='*') break;
       i=1;
-      while (strcmp(string,tname[i]) && (i<=numflds)) i++;
+      while (strcmp(string,fieldat[i].tname) && (i<=numflds)) i++;
       if (i>numflds)
          {
          printf("PPK1: Sort field unknown: %s\n",string);
          continue;
          }
       sortdat[++numsort]=i;
-      needed[i]=1;
+      fieldat[i].needed=1;
       }
                              /* Read in desired output fields */
    while (!feof(config))
@@ -142,32 +138,30 @@ int  flag;
       fscanf(config,"%s",string);
       if (string[0]=='*') break;
       i=1;
-      while (strcmp(string,tname[i]) && (i<=numflds)) i++;
+      while (strcmp(string,fieldat[i].tname) && (i<=numflds)) i++;
       if (i>numflds)
          {
          printf("PPK1: Print field unknown: %s\n",string);
          continue;              /* without using it */
          }
       printdat[++numprint]=i;
-      needed[i]=prneed[i]=1;
+      fieldat[i].needed=fieldat[i].prneed=1;
       prompt(flag,"Enter a display heading for this field: ");
-      display[i]=getstring(config);
+      fieldat[i].display=getstring(config);
       }
    /* print out each sort field and each print field */
    printf("Sort fields defined:\n");
    for (i=1;i<=numsort;i++)
-      printf("%s, ",tname[sortdat[i]]);
+      printf("%s, ",fieldat[sortdat[i]].tname);
    printf("\nPrint fields defined:\n");
    for (i=1;i<=numprint;i++)
-      printf("%s, ",tname[printdat[i]]);
+      printf("%s, ",fieldat[printdat[i]].tname);
    printf("\n");
    }
 
 dosort()         /* Use UNIX sort program to accomplish sort */
 {
-   char cmd[64];
-   sprintf(cmd,"sort -o %s %s",SORTOUT,SORTIN);
-   if (system(cmd)) error("PPK1: Couldn't sort files!\n");
+/* copy temp file 1 to temp file 2 */
 }
 
 /* Copy variable length input file fields to fixed format
@@ -175,46 +169,46 @@ dosort()         /* Use UNIX sort program to accomplish sort */
  */
 copy()
 {
-   int field,i,j,records;
+   int field,i,j,records=0;
    char c,del,*ptr;
-   records=0;
-   /* Open temp file */
+                               /* Open temp file */
    temp1=openup(SORTIN,"w");
                                /* Read source until EOF */
-   while (comma(c=getc(src),!feof(src)))
+   while (c=getc(src),!feof(src))
       {
       ungetc(c,src);
       for (field=1;field<=numflds;field++)
          {
-         ptr=calloc(flength[field]+1,sizeof(char));
-         del=delim[field];
+         ptr=calloc(fieldat[field].flength+1,sizeof(char));
+         del=fieldat[field].delim;
          /* Depending on the value of DEL get either
           * a fixed or a variable length field.
           */
-         if (!del) getfix(src,ptr,flength[field]);
-              else getvar(src,ptr,flength[field],del);
-         if (numer[field])
+         if (!del) getfix(src,ptr,fieldat[field].flength);
+              else getvar(src,ptr,fieldat[field].flength,del);
+         if (fieldat[field].numer)
             { /* fix numeric value - right justify */
             int integer;
             sscanf(ptr,"%d",&integer);
-            sprintf(ptr,"%*d",flength[field],integer);
+            sprintf(ptr,"%*d",fieldat[field].flength,integer);
             }
-         if (needed[field]) dataptr[field]=ptr;
+         if (fieldat[field].needed) fieldat[field].dataptr=ptr;
             else                    cfree(ptr);
          }
                        /* Output all the sort fields first */
       for (field=1;field<=numsort;field++)
-         fprintf(temp1,"%s",dataptr[sortdat[field]]);
+         fprintf(temp1,"%s",fieldat[sortdat[field]].dataptr);
                        /* Followed by all the print fields */
       for (field=1;field<=numsort;field++)
-         fprintf(temp1,"%s",dataptr[printdat[field]]);
-      if (usecr) c=getc(src);       /* if CR at end of record discard it */
-      putc('\n',temp1);             /* Put CR at end of sort line */
-      records=records+1;            /* Count input becords */
-                                    /* Free all space used to store
-                                     *     fields now               */
+         fprintf(temp1,"%s",fieldat[printdat[field]].dataptr);
+      if (usecr) c=getc(src); /* if CR at end of record discard it */
+      putc('\n',temp1);       /* Put CR at end of sort line */
+      records=records+1;      /* Count input becords */
+                              /* Free all space used to store
+                               *     fields now           */
+
       for (i=1;i<=numflds;i++)
-         if (needed[i]) cfree(dataptr[i]);
+         if (fieldat[i].needed) cfree(fieldat[i].dataptr);
       }
    fclose(temp1);;
    if (!records) error("PPK1: Source file empty!\n");
@@ -228,11 +222,12 @@ FILE *openup(file,perms)
 char *file,*perms;
 {
    FILE *fp;
-   if ((fp=fopen(file,perms))==NULL) error("PPK1: Can't open %s\n",file);
+   if ((fp=fopen(file,perms))==NULL)
+      error("PPK1: Can't open %s\n",file);
    return(fp);
 }
 
-/* Print an error and exut with error status */
+/* Print an error and exit with error status */
 error(str,nam)
 char *str,*nam;
 {
@@ -241,77 +236,67 @@ char *str,*nam;
    exit(-1);
 }
 
-getfix(file,ptr,en)           /* get a fixed length field */
+getfix(file,ptr,en)     /* get a fixed length field */
 char *ptr;
 int en;
 FILE *file;
 {
-   int i;
-   i=0;
+   int i=0;
    while (i<en) ptr[i++]=getc(file);  /* read each char */
    ptr[i]=0;
    if (feof(file)) error("PPK1: EOF unexpected!\n");
 }
 
-getvar(file,ptr,en,delim)     /* Get a variable length field */
+getvar(file,ptr,en,delim)   /* Get a variable length field */
 char *ptr,delim;
 int en;
 FILE *file;
 {
-   int i;
+   int i=0;
    char c;
-   i=0;
    while (i<en)    /* read chars to a maximum number */
       {
-      if ((c=getc(file))==delim) break; /* break on delim seen */
+      if ((c=getc(file))==delim) break;
       ptr[i++]=c;
       }
-   while (i<en) ptr[i++]=' ';           /* blank pad resultant field */
+   while (i<en) ptr[i++]=' ';  /* blank pad resultant field */
    ptr[i]=0;
-                                        /* ignore field width overflow */
+                             /* ignore field width overflow */
    while (c!=delim && !feof(file)) c=getc(file);
    if (feof(file)) error("PPK1: EOF unexpected!\n");
 }
 
-dorprt() /* construct the complete report */
+dorprt()
 {
    char c;
    int i,j;
    page=lines=sortlen=sumwidth=0;
    for (i=1;i<=numflds;i++)
-      if (prneed[i])      /* Preallocate required storage */
-         dataptr[i]=calloc(flength[i]+1,sizeof(char));
+      if (fieldat[i].prneed)
+         /* Preallocate required storage */
+         fieldat[i].dataptr=calloc(fieldat[i].flength+1,sizeof(char));
    /* Calculate length of sort fields prepended */
-   for (i=1;i<=numsort;i++) sortlen += flength[sortdat[i]];
+   for (i=1;i<=numsort;i++)
+       sortlen += fieldat[sortdat[i]].flength;
    for (i=1;i<=numprint;i++)
       {
-      /* Calculate print column width for each field and
-       * also total print width fields
-       */
       j=printdat[i];
-      mwidth[i]=max(flength[j],strlen(display[j]));
-      /* The field is considered to be centered within
-       * the field heading, or vice versa.
-       */
+      mwidth[i]=max(fieldat[j].flength,strlen(fieldat[j].display));
       sumwidth+=mwidth[i];
       }
    temp2=openup(SORTOUT,"r");
-   while (comma(c=getc(temp2),!feof(temp2)))   /* Check for EOF */
+   while (c=getc(temp2),!feof(temp2))   /* Check for EOF */
       {
-      ungetc(c,temp2);                  /* Undo illogical UNIX EOF check */
-      if (!lines) ffeed();              /* Formfeed output if lines=0    */
-      getfields();                      /* Get all input fields          */
-      writrecd();                       /* write formatted output        */
-      if (lines>(linpage-14)) lines=0;  /* setup for formfeed            */
+      ungetc(c,temp2);     /* Undo illogical UNIX EOF check */
+      if (!lines) ffeed(); /* Formfeed output if lines=0    */
+      getfields();         /* Get all input fields          */
+      writrecd();          /* write formatted output        */
+      if (lines>(linpage-14)) lines=0;
       }
    fclose(temp2);                       /* close files  */
    fclose(dest);
 }
 
-/* Print a prompt on the standard output
- * provided that 'flag' is zero.
- * Prevents input prompting when in automatic mode.
- */
 prompt(flag,string)
 int flag;
 char *string;
@@ -324,16 +309,15 @@ char *string;
  */
 getfields()
 {
-   int i,j,field,records;
+   int i,j,field,records=0;
    char *ptr;
-   records=0;
    /* disregard sort fields */
    for (i=0;i<sortlen;i++) getc(temp2);
    /* read a field at a time */
    for (i=1;i<=numprint;i++)
       {
-      j=printdat[i];      /* Note now fields are FIXED length */
-      getfix(temp2,dataptr[j],flength[j]);
+      j=printdat[i];   /* Note now fields are FIXED length */
+      getfix(temp2,fieldat[j].dataptr,fieldat[j].flength);
       }
    getc(temp2);    /*discard newline character */
 }
@@ -348,7 +332,7 @@ writrecd()
    for (i=1;i<=numprint;i++)
       {
       /* Print centered field within heading or vice versa */
-      center(0,dataptr[printdat[i]],mwidth[i]);
+      center(0,fieldat[printdat[i]].dataptr,mwidth[i]);
       calcspc(i);
       }
    /* Single, double or triple spaced output */
@@ -367,18 +351,18 @@ ffeed()
    spaces(dest,stdspc-8);
    fprintf(dest,"%s",heading);       /* heading string */
    spaces(dest,stdspc-24);
-   fprintf(dest,"%s",today);         /* print todays date */
-   center(1,heading,columns);        /* underline heading only */
+   fprintf(dest,"%s","Sat 11 Nov 1985 15:25\n");
+   center(1,heading,columns);
    fprintf(dest,"\n\n\n");
    for (i=1;i<=numprint;i++)
-      {             /* print each heading string */
-      center(0,display[printdat[i]],mwidth[i]);
+      {
+      center(0,fieldat[printdat[i]].display,mwidth[i]);
       calcspc(i);
       }
    fprintf(dest,"\n");
    for (i=1;i<=numprint;i++)
-      {             /* underline each heading    */
-      center(1,display[printdat[i]],mwidth[i]);
+      {
+      center(1,fieldat[printdat[i]].display,mwidth[i]);
       calcspc(i);
       }
    fprintf(dest,"\n\n\n");
@@ -390,10 +374,9 @@ char *getstring(file)
 FILE *file;
 {
    char *ptr,c;
-   int i;
-   i=0;
-   ptr=calloc(130,sizeof(char));  /* allocate max. 130 chars */
-   while (comma(c=getc(file),(c==' ' || c=='\n')));
+   int i=0;
+   ptr=calloc(130,sizeof(char));
+   while (c=getc(file),(c==' ' || c=='\n'));
    while (c!='\n' && !feof(file))
       {
       ptr[i++]=c;
@@ -407,8 +390,7 @@ FILE *file;
 spaces(file,num)
 FILE *file;   int num;
 {
-   int i;
-   i=0;
+   int i=0;
    while (i++ < num) putc(' ',file);
 }
 
@@ -423,7 +405,7 @@ int field;
    int x1,x2;
    if (field==numprint) return;
    x1=(columns-sumwidth)/(numprint-1);
-   x2=(columns-numwidth)%(numprint-1);
+   x2=(columns-sumwidth)%(numprint-1);
    if (field<x2) x1++;
    spaces(dest,x1);
 }
@@ -447,9 +429,4 @@ int width,flag;
 /* Quick function to find the maximum of two integers */
 max(a,b)  int a,b;
 { return( a>b ? a : b); }
-
-/* Simulate comma operator */
-comma(exp1,exp2)
-int exp1,exp2;
-{ return( exp2 ); }
 
