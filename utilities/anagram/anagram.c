0c1750699d389c63382d25fb8ae5b5e511cf60e6
Nick Andrew <nick@nick-andrew.net>
2002-06-09 00:04:05 +1000

    *** empty log message ***

---
+++ utilities/anagram/anagram.c
@@ -0,0 +1,49 @@
+#include	"stdio.h"
+
+char	string [20];			/* the string to "anagram" */
+int	string_len;			/* length of same */
+
+main (argc, argv)
+int	argc;
+char	*argv [];
+{
+	if (argc == 1)
+	{
+		printf ("Usage: anagram <string>\n");
+		exit (-1);
+	}
+	strcpy (string, argv [1]);	/* copy string */
+	string_len = strlen (string);	/* find length of it */
+	anagram (0, string_len);	/* find all the anagrams */
+}
+ 
+anagram (start, length)
+int	start;		/* index of the start we are interested in */
+int	length;		/* no. of chars we are interested in */
+{
+	int	i;
+ 
+	if (length == 1)		/* write out the anagram */
+	{
+		for (i = 0; i < string_len; i++)
+			putchar (string [i]);
+		putchar ('\n');
+	}
+	else
+		for (i = 0; i < length; i++)
+			if (i == 0)
+				anagram (start + 1, length - 1);
+			else
+			{
+				char	tmp;	/* temp for swap */
+ 
+				tmp = string [start];	/* swap */
+				string [start] = string [start + i];
+				string [start + i] = tmp;
+				anagram (start + 1, length - 1);
+				tmp = string [start];	/* swap back */
+				string [start] = string [start + i];
+				string [start + i] = tmp;
+			}
+}
+
