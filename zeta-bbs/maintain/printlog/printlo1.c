b14dd9f367c69ed3bc66c140cb960688ef3079fc
Nick Andrew <nick@nick-andrew.net>
2002-06-10 23:44:51 +1000

    Updated 1987-09-25

--- zeta-bbs/maintain/printlog/printlo1.c
+++ zeta-bbs/maintain/printlog/printlo1.c
@@ -1,25 +1,29 @@
 /*
 ** Printlog: Zeta logfile printer program
-** Ver 1.0a on 08-Jun-87
+** Ver 1.1  on 25-Sep-87
 **/
 
 #include <stdio.h>
 
-#define  LOG1     "log/zms"
-#define  LOG2     "xferlog/zms"
+#define  LOG1     "log.zms"
+#define  LOG2     "xferlog.zms"
 
 FILE  *fpin,*fpout;
 char  logx=0,logp=1,flagd=0;
 char  *fout,*fin;
 int   lpp,lines,nlines,nsaved=0,i=0,c;
-char  line1[80],line2[80],linen[66][80];
+char  line1[80],line2[80];
+char  *linen[132],buffer[5280];
 
 main(argc,argv)
 int  argc;
 char *argv[];
 {
-   fout=":l";        /* Alcor C line printer */
-   lpp=58;           /* 60 lines/page - 2    */
+   fout=":l";        /* Line printer       */
+   lpp=58;           /* 60 lines/page - 2  */
+
+   for (i=0;i<66;++i) linen[i] = buffer + 80*i;
+   i=0;
 
    while (--argc) {
       if (**(++argv) == '-')
@@ -42,55 +46,59 @@ char *argv[];
          }
    }
 
-   fin = (logx ? LOG2 : LOG1);
+   if (logx) fin = LOG2; else fin = LOG1;
    if ((fpin=fopen(fin,"r"))==NULL) {
-      printf("printlog: Can't open %s\n",fin);
+      fputs("printlog: Can't open input\n",stderr);
       exit(1);
    }
    if (fgets(line1,80,fpin)==NULL) {
-      printf("printlog: File %s empty\n",fin);
+      fputs("printlog: input is empty\n",stderr);
       exit(2);
    }
    if (fgets(line2,80,fpin)==NULL) {
-      printf("printlog: File %s has only 1 line\n",fin);
+      fputs("printlog: input is empty\n",stderr);
       exit(2);
    }
 
    lines = 0;
    while ((c=getc(fpin))!=EOF)
-      if (c=='\n') lines++;
-   printf("printlog: %d lines in file %s\n",lines,fin);
+      if (c=='\n') ++lines;
+   fputs("printlog: Phase 2 commencing.\n",stderr);
 
    if (lines<lpp) {
-      printf("printlog: No output phase - file too short\n");
+      fputs("printlog: No output - file too short\n",stderr);
       exit(0);
    }
 
    if ((fpout=fopen(fout,"w"))==NULL) {
-      printf("printlog: Can't open output %s\n",fout);
+      fputs("printlog: Can't open output\n",stderr);
       exit(2);
    }
 
-   if (!logp) printf("printlog: Writing to %s\n",fout);
+   fputs(line1,stdout);
+   fputs(line2,stderr);
+
    nlines= lpp*(lines/lpp);
    fclose(fpin);
 
    if ((fpin=fopen(fin,"r"))==NULL) {
-      printf("printlog: Can't re-open %s\n",fin);
+      fputs("printlog: Can't re-open input\n",stderr);
       exit(1);
    }
 
    while(getc(fpin)!='\n'); /* bypass title 1 */
    while(getc(fpin)!='\n'); /* bypass title 2 */
 
-   for(i=0;i<nlines;i++) {
-      if (!(i%lpp)) {
-         fputs(line1,fpout); fputs(line2,fpout);
+   for(i=0;i<nlines;++i) {
+      if (i%lpp == 0) {
+         fputs(line1,fpout);
+         fputs(line2,fpout);
       }
       while (putc(getc(fpin),fpout)!='\n');
    }
 
    /* finished output - save rest of file in memory */
+   nsaved = 0;
    while (fgets(linen[nsaved++],80,fpin)!=NULL);
    fclose(fpout);
    fclose(fpin);
@@ -101,12 +109,14 @@ char *argv[];
       exit(1);
    }
 
+   fputs("Rewriting\n",stdout);
+
    fputs(line1,fpin);
    fputs(line2,fpin);
    for (i=0;i<nsaved;i++) fputs(linen[i],fpin);
    fclose(fpin);
 
-   printf("printlog: finished\n");
+   fputs("printlog: finished\n",stderr);
    exit(0);
 }
 
