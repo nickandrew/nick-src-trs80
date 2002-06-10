1de1bee33d0e90bb6f9040619a6b4c0135ee41a0
Nick Andrew <nick@nick-andrew.net>
2002-06-10 23:29:47 +1000

    Updated 1987-12-24

---
+++ zeta-bbs/maintain/printlog/printlog.asm
@@ -0,0 +1,81 @@
+;Printlog: Print off Zeta caller logs with pagination
+;
+ZETA	EQU	0
+;
+*GET	DOSCALLS:0
+;
+	IF	ZETA
+*GET	EXTERNAL
+*GET	ASCII
+;
+	COM	'<Printlog 1.1a 24-Dec-87>'
+	ORG	PROG_START
+	DEFW	BASE
+	DEFW	THIS_PROG_END
+	DEFW	0
+	DEFW	0
+;End of program load info.
+	ORG	BASE
+	ELSE
+	ORG	5200H
+;
+	ENDIF
+;
+;;*GET	DEBUG:1
+DEBUG	MACRO	#$STR
+	ENDM
+;
+START
+	IF	ZETA
+START1	DEC	HL
+	LD	A,(HL)
+	CP	' '
+	JR	NC,START1
+	INC	HL		;Pseudo start of cmd line
+;
+	LD	SP,START	;There is plenty of stack
+	LD	(_CMDLINE),HL	;Save cmd line pointer
+	LD	HL,1		;Disable redirection
+	LD	(_NOREDIR),HL
+;
+;;	LD	A,(PRIV_1)
+;;	BIT	IS_SYSOP,A
+;;	LD	A,0
+;;	JP	Z,TERMINATE
+;
+	ELSE
+;
+	LD	HL,(HIMEM)
+	LD	SP,HL
+;
+	ENDIF
+;
+*GET	CINIT:1
+*GET	CALL:1
+;
+*GET	PRINTLO1
+;
+*GET	ATOI
+*GET	CTYPE
+;;*GET	FWRITE
+;;*GET	INDEX
+;;*GET	STRCMP
+;;*GET	STRLEN
+;
+	IF	ZETA
+*GET	ROUTINES
+*GET	LIBCZ
+	ELSE
+*GET	LIBC
+	ENDIF
+;
+_CMDLINE	DEFW	4318H
+_NOREDIR	DEFW	0
+;
+HIGHEST	DEFW	$+2
+;
+	IF	ZETA
+THIS_PROG_END	EQU	$
+	ENDIF
+;
+	END	START
