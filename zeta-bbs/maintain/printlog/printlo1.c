2d6494d59dcd4a78e422dc31db246dbee9b8b716
Nick Andrew <nick@nick-andrew.net>
2002-06-10 23:44:53 +1000

    Updated 1987-09-27

--- zeta-bbs/maintain/printlog/printlo1.c
+++ zeta-bbs/maintain/printlog/printlo1.c
@@ -7,6 +7,7 @@
 
 #define  LOG1     "log.zms"
 #define  LOG2     "xferlog.zms"
+#define  OUTPUT   "log.out"
 
 FILE  *fpin,*fpout;
 char  logx=0,logp=1,flagd=0;
@@ -15,12 +16,14 @@ int   lpp,lines,nlines,nsaved=0,i=0,c;
 
 char  *linen[132],buffer[5280];
 char  line1[80],line2[80];
+char  string[20];
+
 
 main(argc,argv)
 int  argc;
 char *argv[];
 {
-   fout=":l";        /* Line printer       */
+   fout= OUTPUT;     /* Line printer       */
    lpp=58;           /* 60 lines/page - 2  */
 
    for (i=0;i<66;++i) linen[i] = buffer + 80*i;
@@ -52,11 +55,13 @@ char *argv[];
       fputs("printlog: Can't open input\n",stderr);
       exit(1);
    }
+
    if (fgets(line1,80,fpin)==NULL) {
       fputs("printlog: input is empty\n",stderr);
       exit(2);
    }
-   if (fgets(line2,80,fpin)==NULL) {
+ 
+  if (fgets(line2,80,fpin)==NULL) {
       fputs("printlog: input is empty\n",stderr);
       exit(2);
    }
@@ -66,6 +71,14 @@ char *argv[];
       if (c=='\n') ++lines;
 
    fputs("printlog: Phase 2 commencing.\n",stderr);
+   fputs(" # lines : ",stderr);
+   itoa(lines,string);
+   fputs(string,stderr);
+
+   fputs(" lpp : ",stderr);
+   itoa(lpp,string);
+   fputs(string,stderr);
+   fputs("\n",stderr);
 
    if (lines<lpp) {
       fputs("printlog: No output - file too short\n",stderr);
@@ -80,8 +93,22 @@ char *argv[];
    fputs(line1,stdout);
    fputs(line2,stderr);
 
+   fputs(" # lines : ",stderr);
+   itoa(lines,string);
+   fputs(string,stderr);
+
+   fputs(" lpp : ",stderr);
+   itoa(lpp,string);
+   fputs(string,stderr);
+   fputs("\n",stderr);
+
    nlines= lpp*(lines/lpp);
    fclose(fpin);
+
+   fputs(" nlines : ",stderr);
+   itoa(nlines,string);
+   fputs(string,stderr);
+   fputs("\n",stderr);
 
    if ((fpin=fopen(fin,"r"))==NULL) {
       fputs("printlog: Can't re-open input\n",stderr);
@@ -94,16 +121,27 @@ char *argv[];
    fputs(line1,stdout);
    fputs(line2,stderr);
    if (lpp != 58) {
-      fputs("Lpp != 58\n",stderr);
+      fputs("Lpp != 58 (up front!)\n",stderr);
       lpp = 58;
    }
 
 
    for(i=0;i<nlines;++i) {
+      fputs("i = ",stderr);
+      itoa(i,string);
+      fputs(string,stderr);
+      fputs("\n",stderr);
+
       if ((i%lpp) == 0) {
          fputs(line1,fpout);
          fputs(line2,fpout);
       }
+
+      if (lpp != 58) {
+         fputs("Lpp != 58\n",stderr);
+         lpp = 58;
+      }
+
       while (putc(getc(fpin),fpout)!='\n');
    }
 
