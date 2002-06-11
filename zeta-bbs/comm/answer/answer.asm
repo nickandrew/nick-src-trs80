b6d577b1c480f9a8b4cd1019aa217509d7317039
Nick Andrew <nick@nick-andrew.net>
2002-06-11 21:22:15 +1000

    Updated 1990-05-20

--- zeta-bbs/comm/answer/answer.asm
+++ zeta-bbs/comm/answer/answer.asm
@@ -1,6 +1,14 @@
 ;answer: telephone answer & initial connection program
 ;
-; 3.3p: 	Lock out V21 callers
+; 4.0d 20 May 90 Remove reference to ACSnet and Fidonet addresses
+; 4.0c 24 Mar 90 Use ftalk2 instead of ftalk when answering Fidonet
+; 4.0b 12 Aug 89 Re-enable V21 carrier, and give a short message when
+;	somebody attempts to logon.
+; 4.0a 24 Jul 89 Stop answering phone during incoming call
+;	(When modem says "RING", it is eventually followed by either a
+;	"CONNECT" or "NO CARRIER" ... so go into another checking loop)
+; 4.0  02 Jul 89 Restructured somewhat...
+; 3.3p  	Lock out V21 callers
 ;		Ensure modem always talks to us at v22
 ;		Change "modem online, start" to start at current rate
 ; 3.3o:		Add "waiting for carrier" message
@@ -19,20 +27,21 @@ $TA	EQU	0FF38H		;Type ahead.
 	DEFW	TERMINATE
 ;End of program load info.
 ;
-	COM	'<Answer 3.3p 21-Jun-89>'
+	COM	'<Answer 4.0d 20 May 90>'
 	ORG	BASE+100H
-START	LD	SP,START
-;
+START
+	LD	SP,START
 	LD	A,(SYS_STAT)	;check status
 	BIT	SYS_LOGDIN,A
-	JR	Z,STAT_OK
+	JR	Z,ANS_01
+;
 	LD	HL,M_42
 	LD	DE,$2
-	CALL	MESS_0
+	CALL	MESS_0		;Print a cute message
 	LD	A,2
-	JP	TERMINATE	;if logged on. ???
+	JP	TERMINATE	;if already logged on
 ;
-STAT_OK
+ANS_01
 	LD	A,1		;Set CPU fast.
 	OUT	(0FEH),A
 ;
@@ -41,12 +50,6 @@ STAT_OK
 ;
 	LD	HL,CD_STAT
 	RES	CDS_DISCON,(HL)
-;
-;;	LD	HL,SYS_STAT
-;;	RES	0,(HL)
-;;	RES	1,(HL)
-;;	RES	2,(HL)
-;;	CALL	TEL_HANGUP	;reset dtr
 ;
 	CALL	DTR_OFF
 	CALL	SET_1200	;For netcomm
@@ -62,13 +65,7 @@ STAT_OK
 ;
 	CALL	RESET_DCB
 ;
-;Set all new devices to point to vdu
-	LD	HL,$DO
-	LD	($STDOUT),HL
-	LD	($STDOUT_DEF),HL
-	LD	HL,$KI
-	LD	($STDIN),HL
-	LD	($STDIN_DEF),HL
+	CALL	SET_SCREEN	;Std dev point to screen
 ;
 	LD	A,0A2H		;Trs-80 for sysop
 	LD	(TFLAG2),A
@@ -86,15 +83,14 @@ STAT_OK
 	LD	DE,$DO
 	CALL	MESS_0
 ;
-ANSWER
 ;if TEST mode don't signal HANGUP. Just reset mode
 	LD	HL,SYS_STAT
 	BIT	6,(HL)
-	JR	Z,NOT_TEST
+	JR	Z,ANS_02
 	RES	6,(HL)		;Reset TEST mode.
-	JR	NOT_MSG
+	JR	ANS_03
 ;
-NOT_TEST
+ANS_02
 ;signal termination of previous call.
 	CALL	T_STR		;time in string
 	LD	HL,TIMDAT
@@ -102,70 +98,62 @@ NOT_TEST
 	LD	HL,M_HUP
 	CALL	LOG_MSG
 ;
-NOT_MSG
+ANS_03
 ;Wait for call to come in
 CDLOOP
 	LD	HL,M_WAITING
 	LD	DE,$DO
 	CALL	MESS_0
-CDLOOP_0
-	CALL	MODEM_GETC
+CDL_00
+	CALL	MODEM_CHAR
 	OR	A
 	CP	'2'		;ring signal
-	JR	Z,CD_RING
+	JR	Z,CDL_02	;Ringing
 	CP	'3'		;no carrier
 	JP	Z,CD_NOCARR
 	CP	'1'		;1=Conn 300. 10=Conn 2400
-	JP	Z,CD_300
+	JP	Z,CD_VAR
 	CP	'5'		;connect 1200 (/75?)
 	JP	Z,CD_1200
 	CP	'0'
-	JR	Z,CDLOOP_0	;ok
+	JR	Z,CDL_00	;ok
 ;
 	LD	A,(CD_MODE)
 	AND	0FH
 	CP	4
-	JR	NZ,CDL_1
+	JR	NZ,CDL_01
 	LD	HL,M_BADMODE
-	CALL	LOG_MSG_2
+	CALL	LOG_MSG
 	XOR	A
 	LD	(CD_MODE),A
-CDL_1
+CDL_01
 ;
-	CALL	SP_CMD
-;Do ring timing. Count 1/5 second intervals.
-	LD	A,(RING_TICK)
-	LD	B,A
-	LD	A,(TICKER)
-	AND	0F8H		;ignore lower 3 bits.
-	CP	B
-	JR	Z,CDLOOP_0
-	LD	(RING_TICK),A
-	LD	HL,RING_TIME
-	INC	(HL)
-	LD	HL,3C3FH
-	INC	(HL)
-	JR	CDLOOP_0
+	CALL	CHECK_CRON
+	CALL	CHECK_KEYB
 ;
-CD_RING
-	LD	HL,M_RING
-	LD	DE,$DO
-	CALL	MESS_0
+	CALL	TIME_RING
+	JR	CDL_00
 ;
-	XOR	A
-	LD	(RING_TIME),A
-	JR	CDLOOP_0
+; Telephone rang, so wait for a connect or a no carrier
+CDL_02
+	CALL	CD_RING		;Print ring message, set ring time to 0
+CDL_03
+	CALL	MODEM_CHAR
+	CP	'2'		;ring signal
+	JR	Z,CDL_02
+	CP	'3'		;no carrier
+	JP	Z,CD_NOCARR
+	CP	'1'		;1=Conn 300. 10=Conn 2400
+	JP	Z,CD_VAR
+	CP	'5'		;connect 1200 (/75?)
+	JP	Z,CD_1200
+	CP	'0'
+	JR	Z,CDL_00	;ok
+;Do not check cron here, as somebody is calling us!
+	CALL	CHECK_KEYB
 ;
-LOG_PICKUP
-	LD	HL,PUP_TIME	;Record pickup time/date
-	CALL	446DH
-	LD	HL,PUP_DATE
-	CALL	4470H
-;
-	CALL	T_STR		;time in string
-	LD	HL,TIMDAT	;{dd-mmm-yy ....
-	CALL	LOG_MSG
-	RET
+	CALL	TIME_RING
+	JR	CDL_03
 ;
 ; - record actions then rerun answer.
 CD_NOCARR
@@ -183,17 +171,7 @@ CD_NOCARR
 	LD	DE,$DO
 	CALL	MESS_0
 ;
-	JP	CDLOOP_0
-;
-;clear carrier detected flags
-GO_AWAY
-	LD	HL,SYS_STAT
-;;	RES	0,(HL)		;on hook
-;;	RES	1,(HL)		;carrier was detected?
-;;	RES	2,(HL)		;carrier considered lost?
-	LD	HL,EXITSYS	;Loop back..
-	CALL	OVERLAY
-	JP	$		;Overlay failed, rats!
+	JP	CDL_00
 ;
 CD_1200
 	CALL	SET_1200
@@ -216,10 +194,11 @@ CD_1200_75
 	CALL	LOG_MSG
 	JR	CDFOUND
 ;
-CD_300
-	CALL	MODEM_GETC
+;Modem detected carrier at either 300 BPS or 2400 BPS.
+CD_VAR
+	CALL	MODEM_CHAR
 	OR	A
-	JR	Z,CD_300
+	JR	Z,CD_VAR
 	CP	CR
 	JR	Z,CD_300_2
 	CP	'0'		;'10'=connect 2400
@@ -234,7 +213,19 @@ CD_300_2
 	CALL	MESS_0
 	LD	HL,M_FDCARR_V21
 	CALL	LOG_MSG
-	JR	CDFOUND
+;
+	LD	HL,M_TOOSLOW
+	LD	DE,$2
+	CALL	MESS_0
+;
+	LD	A,100		;Delay 10 full seconds
+	CALL	SEC10
+;
+	CALL	DTR_OFF		;Boot them off, quick
+;
+	LD	A,10		;Wait 1 second with DTR off.
+	CALL	SEC10
+	JP	ANS_01		;Not structured, but re-init.
 ;
 CD_2400
 	CALL	SET_2400
@@ -246,15 +237,15 @@ CD_2400
 	CALL	LOG_MSG
 	JR	CDFOUND
 ;
+;A carrier was found at a valid speed. We have set our comms speed.
 CDFOUND
 	LD	A,1
 	CALL	SEC10
 	CALL	CARR_DETECT
 ;
-	LD	A,20		;Wait 3 seconds
+	LD	A,20		;Wait 2 seconds
 	CALL	SEC10
 	CALL	MODEM_FLUSH
-;
 ;
 	CALL	T_STR
 	LD	HL,TIMDAT	;Output time/date
@@ -268,7 +259,7 @@ CDFOUND
 	LD	A,(TICKER)
 	LD	C,A
 SWAIT_1	PUSH	DE
-	CALL	MODEM_GETC
+	CALL	MODEM_CHAR
 SWAIT_1Z
 	CP	TSYNC
 	JP	Z,FIDO_CALL
@@ -281,6 +272,7 @@ SWAIT_1Z
 	CP	' '
 	JR	Z,SWAIT_1B
 	JR	SWAIT_2
+;
 SWAIT_1B
 	LD	HL,M_DUMMY
 SWAIT_1A
@@ -288,13 +280,11 @@ SWAIT_1A
 	OR	A
 	JR	Z,SWAIT_2
 	CALL	MODEM_PUTC
+	CALL	MODEM_CHAR	;Get character if any
+	CP	TSYNC		;Only check for TSYNC
+	JP	Z,FIDO_CALL	;Go if found one!
 	INC	HL
-;;	CALL	MODEM_GETC
-;;	OR	A
-;;	JR	Z,SWAIT_1A
 	JR	SWAIT_1A
-;
-;;	JR	SWAIT_1Z
 ;
 SWAIT_2
 	POP	DE
@@ -315,7 +305,7 @@ NOT_FIDO
 	LD	A,0BFH		;LF required terminal
 	LD	(TFLAG2),A
 	LD	HL,APB		;list the intro
-	CALL	LIST
+	CALL	LISTFILE
 ;
 	JR	RUN_LOGON
 ;
@@ -325,16 +315,6 @@ RUN_LOGON
 	CALL	OVERLAY		;execute LOGON prog
 	JP	$		;Failed. Darn.
 ;
-MODEM_ACK
-	LD	A,(TICKER)
-	ADD	A,40
-	LD	B,A
-MA_01	LD	A,(TICKER)
-	CP	B
-	RET	Z
-	CALL	MODEM_GETC_SHOW
-	JR	MA_01
-;
 T_STR
 	LD	HL,DATE
 	CALL	X_TODAY
@@ -342,18 +322,48 @@ T_STR
 	CALL	446DH
 	RET
 ;
-SP_CMD
-SPC_1
+;Do ring timing. Count 1/5 second intervals.
+TIME_RING
+	LD	A,(RING_TICK)
+	LD	B,A
+	LD	A,(TICKER)
+	AND	0F8H		;ignore low 3 bits
+	CP	B
+	RET	Z
+	LD	(RING_TICK),A
+	LD	HL,RING_TIME
+	INC	(HL)
+	LD	HL,3C3FH
+	INC	(HL)
+	RET
+;
+CD_RING
+	LD	HL,M_RING
+	LD	DE,$DO
+	CALL	MESS_0
+;
+	XOR	A
+	LD	(RING_TIME),A
+	RET
+;
+LOG_PICKUP
+	LD	HL,PUP_TIME	;Record pickup time/date
+	CALL	446DH
+	LD	HL,PUP_DATE
+	CALL	4470H
+;
+	CALL	T_STR		;time in string
+	LD	HL,TIMDAT	;{dd-mmm-yy ....
+	CALL	LOG_MSG
+	RET
+;
+CHECK_CRON
 	LD	A,(4041H)	;sec
 	OR	A
-	JR	NZ,NOT_CRON
+	RET	NZ
 	LD	A,(4042H)	;min
 	OR	A
-	JR	Z,IS_CRON	;Run every hour
-NOT_CRON
-	JR	NOT_TIME
-;
-IS_CRON
+	RET	NZ
 ;
 	LD	HL,0
 	LD	(DISCON),HL
@@ -375,66 +385,6 @@ IS_CRON
 ;
 	LD	DE,0		;reset counters
 	RET
-;
-NOT_TIME
-	LD	DE,$KI
-	CALL	$GET
-	CP	1FH		;CLEAR key
-	JP	Z,SP_FUNC_JP
-	RET
-;
-;Dialout time!
-DIAL_OUT
-	LD	HL,POLL_NABA1
-	LD	(POLL_STRING),HL
-	LD	HL,PH_NABA1
-	CP	'C'
-	JR	Z,DIAL_0A
-	LD	HL,POLL_NABA2
-	LD	(POLL_STRING),HL
-	LD	HL,PH_NABA2
-DIAL_0A	PUSH	HL
-	CALL	FIDO_LOGIN
-	CALL	SET_2400
-DIAL_0B	LD	HL,MODEM_AT
-	CALL	MODEM_PUTS
-	LD	HL,MODEM_1F
-	CALL	MODEM_PUTS
-	POP	HL		;Phone number string
-	CALL	MODEM_PUTS
-;
-DIAL_1	CALL	MODEM_GETC
-	OR	A
-	JR	Z,DIAL_1
-	CP	'0'
-	JR	Z,DIAL_1
-	CP	'3'		;No carrier
-	JP	Z,GO_AWAY	;Run exitsys
-	CP	'1'		;v21/22bis (assume 22bis)
-	JR	Z,DIAL_24
-	CP	'5'
-	JR	Z,DIAL_12
-	JR	DIAL_1
-;
-DIAL_12
-	LD	HL,M_FDCARR_V22
-	LD	DE,$DO
-	CALL	MESS_0
-	LD	HL,M_FDCARR_V22
-	CALL	LOG_MSG
-	CALL	SET_1200
-	JR	DIAL_POLL
-DIAL_24
-	LD	HL,M_FDCARR_BIS
-	LD	DE,$DO
-	CALL	MESS_0
-	LD	HL,M_FDCARR_BIS
-	CALL	LOG_MSG
-	CALL	SET_2400
-DIAL_POLL
-	LD	HL,(POLL_STRING)
-	CALL	OVERLAY
-	JP	$
 ;
 HAYES
 	LD	HL,M_HAYES
@@ -465,6 +415,13 @@ CALL_FND
 	CALL	MODEM_PUTS	;answer phone
 	JP	CDLOOP		;wait for results
 ;
+CHECK_KEYB
+	LD	DE,$KI
+	CALL	$GET
+	CP	1FH		;CLEAR key
+	JP	Z,SP_FUNC_JP
+	RET
+;
 ;Special function
 SP_FUNC_JP
 	POP	AF		;return address
@@ -477,7 +434,7 @@ GET_FN	LD	HL,M_SPPMT
 	LD	HL,STRING
 	LD	B,2
 	CALL	40H
-	JP	C,CDLOOP	;=do nothing
+	JP	C,CDLOOP	;break = do nothing
 	LD	A,(HL)
 	CP	'!'		;=shell esc
 	JP	Z,SHELL_ESC
@@ -490,23 +447,19 @@ GET_FN	LD	HL,M_SPPMT
 	AND	5FH
 GF_0
 	LD	(HL),A
-	CP	'A'		;=answer via ATA
-	JR	Z,CALL_FND
-	CP	'C'
-	JP	Z,DIAL_OUT	;Dial Naba as acsgate
+	CP	'A'
+	JR	Z,CALL_FND	;Answer via ATA
 	CP	'H'
 	JP	Z,HAYES		;Hayes command.
-	CP	'N'
-	JP	Z,DIAL_OUT	;Dial Naba
-	CP	'O'		;Modem is online!
-	JP	Z,ONLINE
-	CP	'S'		;=shutdown
-	JR	Z,SHUTDOWN
-	CP	'T'		;=test system
-	JR	Z,SET_TEST
+	CP	'O'
+	JP	Z,ONLINE	;Modem is online!
+	CP	'S'
+	JR	Z,SHUTDOWN	;Shut down the system
+	CP	'T'
+	JR	Z,SET_TEST	;Test system
 	CP	'1'
 	JR	NZ,GF_1
-	CALL	SET_1200
+	CALL	SET_1200	;Set speed to 1200
 	JR	ATTN
 GF_1	CP	'2'
 	JR	NZ,GF_2
@@ -521,18 +474,13 @@ ATTN	LD	HL,MODEM_AT
 GF_3
 	JR	SP_FUNC
 ;
-SET_TEST
 ;set TEST mode. run LOGON for sysop.
+SET_TEST
 	LD	HL,SYS_STAT
 	SET	6,(HL)
 ;
 	CALL	SET_1200
 	CALL	SET_STDDEV
-;
-;;set serial device inaccessible. Why?????????????
-;;	LD	A,0
-;;	LD	(CD_STAT),A
-;;(helps with Ctrl-S processing).	?????????
 ;
 	LD	HL,MODEM_ATH1	;stop it answering
 	CALL	MODEM_PUTS
@@ -561,7 +509,7 @@ SHELL_ESC			;A shell escape.
 	CALL	40H
 	CALL	NC,CALL_PROG
 	CALL	SYSTEM_LOGOUT	;shell escape
-	JP	CDLOOP
+	JP	CDLOOP		;Wait for carrier again
 ;
 SYSTEM_ESC
 	LD	HL,SYSTEM
@@ -573,13 +521,13 @@ SYSTEM_ESC
 	CALL	40H
 	CALL	NC,CALL_PROG
 	CALL	SYSTEM_LOGOUT	;shell escape
-	JP	CDLOOP
+	JP	CDLOOP		;Wait for carrier again
 ;
 CRON
 	LD	HL,SYSTEM
 	CALL	SYSTEM_LOGON
 ;
-	LD	HL,CRONCMD	;Run cron now!
+	LD	HL,CMD_CRON	;Run cron now!
 	CALL	CALL_PROG
 ;
 	CALL	SYSTEM_LOGOUT
@@ -640,6 +588,7 @@ SYSTEM_LOGOUT
 	LD	HL,CD_STAT
 	RES	CDS_DISCON,(HL)
 ;
+	CALL	SET_1200
 	LD	HL,MODEM_ATZ
 	CALL	MODEM_PUTS
 ;
@@ -653,8 +602,16 @@ RESET_DCB
 	LD	($KBD+1),HL
 	LD	HL,4505H
 	LD	($VDU+1),HL
-;;	LD	HL,SYS_STAT
-;;	RES	3,(HL)
+	RET
+;
+SET_SCREEN
+;Set all new devices to point to vdu
+	LD	HL,$DO
+	LD	($STDOUT),HL
+	LD	($STDOUT_DEF),HL
+	LD	HL,$KI
+	LD	($STDIN),HL
+	LD	($STDIN_DEF),HL
 	RET
 ;
 ZERO_TA
@@ -695,9 +652,6 @@ ROUTE_DEV
 	LD	HL,($2+1)
 	LD	($VDU+1),HL
 ;
-;;	LD	HL,SYS_STAT	;Set 'DEVICES ROUTED'
-;;	SET	3,(HL)
-;
 	CALL	SET_STDDEV
 ;
 	CALL	ZERO_TA
@@ -720,8 +674,13 @@ FIDO_CALL			;handle a call from Fido
 	CALL	MESS_0
 ;
 	LD	HL,CMD_GETPKT
-	CALL	OVERLAY
-	JP	$		;Overlay failed. darn.
+	CALL	CALL_PROG
+	CALL	SYSTEM_LOGOUT
+	JP	CDLOOP
+;
+;;	LD	HL,CMD_GETPKT
+;;	CALL	OVERLAY
+;;	JP	$		;Overlay failed. darn.
 ;
 FIDO_LOGIN
 	LD	HL,FIDO_NAME
@@ -733,9 +692,6 @@ FIDO_LOGIN
 	LD	(PRIV_1),A
 	LD	A,09H
 	LD	(PRIV_2),A
-;;	LD	HL,SYS_STAT
-;;	SET	SYS_LOGDIN,(HL)
-;;	SET	SYS_TEST,(HL)
 	LD	HL,2
 	LD	(USR_NUMBER),HL
 ;
@@ -743,7 +699,7 @@ FIDO_LOGIN
 ;"Fido Network" is now logged in.
 	RET
 ;
-MODEM_GETC
+MODEM_CHAR
 	IN	A,(RDSTAT)	;check status
 	BIT	DAV_BIT,A
 	LD	A,0
@@ -768,17 +724,75 @@ MP_1	IN	A,(RDSTAT)
 	OUT	(WRDATA),A
 	RET
 ;
-MODEM_PUTS
-	CALL	MODEM_GETC_SHOW
+MODEM_GPUTS
+;Output a string. Ignore its echo. Read a reply from the modem in HL.
+MG_01
 	LD	A,(HL)
 	OR	A
-	JP	Z,MODEM_ACK
+	JR	Z,MG_02
+	CALL	MODEM_PUTC
+	CALL	MODEM_GCHAR_1		;0.1 second maximum wait
+	INC	HL
+	JR	MG_01
+MG_02
+	LD	B,64			;A maximum length
+MG_03
+	CALL	MODEM_GCHAR_10		;1 second maximum wait
+	JR	C,MG_04			;No response
+	LD	(DE),A
+	INC	DE
+	DJNZ	MG_03
+MG_04	XOR	A
+	LD	(DE),A
+	RET
+;
+MODEM_GCHAR_1
+	LD	A,(TICKER)
+	ADD	A,4
+	LD	C,A
+MG1_01	LD	A,(TICKER)
+	CP	C
+	SCF
+	RET	Z
+	CALL	MODEM_CHAR
+	JR	Z,MG1_01
+	OR	A
+	RET
+;
+MODEM_GCHAR_10
+	LD	A,(TICKER)
+	ADD	A,40
+	LD	C,A
+MG10_01	LD	A,(TICKER)
+	CP	C
+	SCF
+	RET	Z
+	CALL	MODEM_CHAR
+	JR	Z,MG10_01
+	OR	A
+	RET
+;
+MODEM_PUTS
+	CALL	MODEM_CHAR_SHOW
+	LD	A,(HL)
+	OR	A
+	JR	Z,MODEM_ACK
 	CALL	MODEM_PUTC
 	INC	HL
 	JR	MODEM_PUTS
 ;
-MODEM_GETC_SHOW
-	CALL	MODEM_GETC
+MODEM_ACK
+	LD	A,(TICKER)
+	ADD	A,40
+	LD	B,A
+MA_01	LD	A,(TICKER)
+	CP	B
+	RET	Z
+	CALL	MODEM_CHAR_SHOW
+	JR	MA_01
+;
+MODEM_CHAR_SHOW
+	CALL	MODEM_CHAR
 	OR	A
 	RET	Z
 	CP	LF
@@ -835,12 +849,33 @@ SET_2400
 	OUT	(WRSTAT),A
 	RET
 ;
+;List a file to the dual output
+LISTFILE
+	LD	DE,FCB_LIST
+	CALL	EXTRACT
+	LD	HL,BUF_LIST
+	LD	B,0
+	CALL	DOS_OPEN_EX
+	RET	NZ
+LF_01
+	LD	DE,FCB_LIST
+	CALL	$GET
+	JR	NZ,LF_02
+	LD	DE,$2
+	CALL	$PUT
+	JR	LF_01
+;
+LF_02
+	LD	DE,FCB_LIST
+	CALL	DOS_CLOSE
+	RET
+;
 ;Include common routines.
 *GET	ROUTINES.LIB
 ;
 MODEM_1A	DEFM	'AT&FB2&T5',CR,0
 MODEM_1B	DEFM	'AT v0 x1 h0 s0=3 s2=255 s11=8',CR,0
-MODEM_1E	DEFM	'AT m1 b14 &w',CR,0
+MODEM_1E	DEFM	'AT m1 b2 &w',CR,0
 MODEM_1F	DEFM	'ATB0M1',CR,0	;1200 or 2400 bps
 MODEM_AT	DEFM	'AT',CR,0
 MODEM_ATO	DEFM	'ATO',CR,0
@@ -849,9 +884,9 @@ MODEM_ATH0	DEFM	'ATH0',CR,0
 MODEM_ATZ	DEFM	'ATZ',CR,0
 MODEM_4		DEFM	'ATA',CR,0
 ;
-M_FIDO	DEFM	CR,CR,'** Fido transfer underway',CR,0
-M_RING	DEFM	' Ring ... ',0
-M_HAYES	DEFM	'Hayes cmd: ',0
+M_FIDO		DEFM	CR,CR,'** Fidonet transfer underway **',CR,0
+M_RING		DEFM	' Ring ... ',0
+M_HAYES		DEFM	'Hayes command: ',0
 FIDO_NAME	DEFM	'Fido Network',CR,0
 LAST_TICK	DEFB	0
 RING_TIME	DEFB	0
@@ -866,67 +901,64 @@ M_SPFUNC
 	DEFM	'"1": Set 1200 bps     "2" Set 2400 bps    "3" Set 300 bps',CR
 	DEFM	CR,0
 ;
-M_SPPMT
-	DEFM	'Function > ',0
+M_TOOSLOW
+	DEFM	CR,CR
+	DEFM	'Thank you for calling Zeta.  This system no longer supports',CR
+	DEFM	'V21 (300 bps) callers.  Modem speeds supported are:',CR
+	DEFM	'    V22bis  (2400 bps)',CR
+	DEFM	'    V22     (1200 bps)',CR
+	DEFM	'and V23     (1200/75 bps Ascii)',CR
+	DEFM	CR
+	DEFM	'For more information about Zeta, please send a SASE to:',CR
+	DEFM	'        Zeta Microcomputer Software',CR
+	DEFM	'        P.O Box 177,',CR
+	DEFM	'        Riverstone NSW 2765',CR
+	DEFM	'and mention that you attempted to call at 300 bps.',CR
+	DEFM	CR
+	DEFM	CR,0
 ;
-M_SHUTDN
-	DEFM	'Shutdown.',CR,3,0	;close log
+M_SPPMT		DEFM	'Function > ',0
 ;
-M_42
-	DEFM	CR,'      42',CR,CR,0
+M_SHUTDN	DEFM	'Shutdown.',CR,3,0	;close log
 ;
-M_NOCARR DEFM	'No carrier found',CR,0
-M_FDCARR_V21
-	DEFM	'Found V21 carrier',CR,0
-M_FDCARR_V22
-	DEFM	'Found V22 carrier',CR,0
-M_FDCARR_BIS
-	DEFM	'Found V22bis carrier',CR,0
-M_FDCARR_V23
-	DEFM	'Found V23 carrier',CR,0
+M_42		DEFM	CR,'      42',CR,CR,0
+;
+M_NOCARR	DEFM	'No carrier found',CR,0
+M_FDCARR_V21	DEFM	'Found V21 carrier!',CR,0
+M_FDCARR_V22	DEFM	'Found V22 carrier',CR,0
+M_FDCARR_BIS	DEFM	'Found V22bis carrier',CR,0
+M_FDCARR_V23	DEFM	'Found V23 carrier',CR,0
 ;
 CMD_LOGON	DEFM	'Logon',0
-EXITSYS		DEFM	'Exitsys',0
-CRONCMD		DEFM	'Cron',0
-CMD_GETPKT	DEFM	'Getpkt',0
+CMD_EXITSYS	DEFM	'Exitsys',0
+CMD_CRON	DEFM	'Cron',0
+CMD_GETPKT	DEFM	'Ftalk2 -l',0
 ;
-POLL_STRING	DEFW	0
-POLL_NABA1	DEFM	'Ftalk -c 0/0',0	;as acsg
-POLL_NABA2	DEFM	'Ftalk -c 713/606',0
-POLL_ACSGATE	DEFM	'Ftalk -c 713/603',0
-PH_NABA1
-	DEFM	'AT B0 M1 DT6287030',CR,0
-PH_NABA2
-	DEFM	'AT B0 M1 DT6287030',CR,0
-PH_ACSGATE
-	DEFM	'at b2 m1 dt2111406',CR,0
-;
-;
-M_RUNNING DEFM	CR,'Running Answer',CR,0
+M_RUNNING	DEFM	CR,'Running Answer',CR,0
 ;
 ;M_hup contains 01H byte which closes & re-opens
 ;system log file.
-M_HUP	DEFM	'Hangup',CR,CR,1,0
+M_HUP		DEFM	'Hangup',CR,CR,1,0
 ;
 TIMDAT
-DATE	DEFM	'DD-MMM-YY  '
-TIME	DEFM	'HH:MM:SS : ',0
+DATE		DEFM	'DD-MMM-YY  '
+TIME		DEFM	'HH:MM:SS : ',0
 ;
-APB	DEFM	'APB.ZMS',CR
+APB		DEFM	'APB.ZMS',CR
 ;
-M_ZETA	DEFM	CR,LF,'Welcome to Zeta, Fidonet 3:713/602,'
-	DEFM	' ACSnet 713.602@fidogate.fido.oz'
-	DEFM	CR,LF,CR,LF,0
-M_DUMMY	DEFM	'Please be patient!',CR,LF,0
-SYSTEM	DEFM	'System',0
-NICK	DEFM	'Nick Andrew',0
-M_ESC	DEFM	CR,'System escape >',0
-M_BADMODE
-	DEFM	'Mode number 4 detected!',CR,0
-M_WAITING
-	DEFM	CR,'Waiting for call',CR,0
+M_ZETA		DEFM	CR,LF,CR,LF
+		DEFM	'Welcome to Zeta, O modem user!'
+		DEFM	CR,LF,CR,LF,0
+M_DUMMY		DEFM	'Please be patient!',CR,LF,0
+SYSTEM		DEFM	'System',0
+NICK		DEFM	'Nick Andrew',0
+M_ESC		DEFM	CR,'System escape >',0
+M_BADMODE	DEFM	'Mode number 4 detected!',CR,0
+M_WAITING	DEFM	CR,'Waiting for call',CR,0
 ;
-STRING	DC	64,0
+STRING		DC	64,0
+FCB_LIST	DEFS	32
+BUF_LIST	DEFS	256
 ;
 THIS_PROG_END	EQU	$
 ;
