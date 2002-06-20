b8d3f7dca9ec724bdefca541fe0ec1bd611b81b8
Nick Andrew <nick@nick-andrew.net>
2002-06-20 20:50:41 +1000

    Updated 1987-05-18

---
+++ include/include/ascii.asm
@@ -0,0 +1,17 @@
+;ASCII: Definitions for control codes.
+;Last updated 18-May-87.
+;
+*LIST	OFF
+NULL	EQU	0
+ETX	EQU	3
+BELL	EQU	7
+BS	EQU	8
+TAB	EQU	9
+LF	EQU	0AH
+NL	EQU	0AH
+CR	EQU	0DH
+CH_XON	EQU	11H	;^Q
+CH_XOFF	EQU	13H	;^S
+DEL	EQU	7FH
+;
+*LIST	ON
