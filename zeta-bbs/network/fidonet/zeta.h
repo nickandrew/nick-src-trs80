6a1e7ba85f7b4781575ad15e064cbd2404d1a5e4
Nick Andrew <nick@nick-andrew.net>
2002-06-20 23:08:29 +1000

    1990-07-31

---
+++ zeta-bbs/network/fidonet/zeta.h
@@ -0,0 +1,19 @@
+/*  zeta.h : Vital statistics for Zeta
+**	@(#) zeta.h 31 Jul 90
+*/
+
+/* Where we are in the scheme of things */
+
+#define ZETA_NET	713
+#define ZETA_NODE	602
+#define ZETA_ZONE	3
+
+#define ACS_NET		7802
+#define ACS_NODE	603
+#define ACS_ZONE	8
+
+#define HOST_NET	713
+#define HOST_NODE	600
+#define HOST_ZONE	3
+
+/* end of zeta.h */
