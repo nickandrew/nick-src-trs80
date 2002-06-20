4e586561f260660779e06dd559b1f9b3ca510448
Nick Andrew <nick@nick-andrew.net>
2002-06-20 21:29:08 +1000

    Merged all useful things from previous versions

--- include/include/doscalls.asm
+++ include/include/doscalls.asm
@@ -1,7 +1,7 @@
-;doscalls: dos type equates of 20-Nov-87
-*LIST OFF
-;
+; doscalls: dos type equates of 2002-06-20
 ; Also common Dos error messages & numbers.
+;
+*LIST OFF
 ;
 MODEL1	EQU	-1	;Change to 0 for model III.
 			;Change to -1 for model I.
@@ -9,29 +9,49 @@ MODEL1	EQU	-1	;Change to 0 for model III.
 MODEL3	EQU	.NOT.MODEL1
 ;
 ;
+CURSOR		EQU	4020H	; cursor position?
 DOS		EQU	402DH	;no error exit
-DOS_DISP_ERROR	EQU	4030H	;error displayed exit
 DOS_NOERROR	EQU	402DH	;no error
-DOS_ERROR	EQU	4409H	;error exit
-DOS_CALL	EQU	4419H	;dos-call
+DOS_DISP_ERROR	EQU	4030H	;error displayed exit
+DOS_COMMAND	EQU	4405H	;enter DOS and execute a command
+DOS_ERROR	EQU	4409H	;DOS error exit
+DOS_DEBUG	EQU	440DH	;enter DEBUG
+DOS_ENQUEUE	EQU	4410H	;enqueue user timer interrupt routine
+DOS_DEQUEUE	EQU	4413H	;dequeue user timer interrupt routine
+DOS_ROTATE	EQU	4416H	;Keep drives rotating
+DOS_CALL	EQU	4419H	;dos-call execute a DOS command and return
 DOS_EXTRACT	EQU	441CH	;filename extract
 DOS_OPEN_NEW	EQU	4420H	;open new/exist file
 DOS_OPEN_EX	EQU	4424H	;open existing file
 DOS_CLOSE	EQU	4428H	;close file
-DOS_KILL	EQU	442CH	;kill file
+DOS_KILL	EQU	442CH	;kill FCB's associated file
+DOS_LOAD	EQU	4430H	;load a program file
+DOS/EXECUTE	EQU	4433H	;load and commence execution of a program file
 DOS_READ_SECT	EQU	4436H	;read file's record
 DOS_WRIT_SECT	EQU	4439H	;write file's record
 DOS_REWIND	EQU	443FH	;set NEXT to 0/0/0.
 DOS_POSIT	EQU	4442H	;position to relrec
 DOS_BACK_RECD	EQU	4445H	;position back 1 recd
 DOS_POS_EOF	EQU	4448H	;position to END
+DOS_ALLOCATE	EQU	444BH	;allocate file space
 DOS_POS_RBA	EQU	444EH	;position to RBA
 DOS_WRITE_EOF	EQU	4451H	;from fcb to directory
-DOS_POWERUP	EQU	445BH	;spin drive
+DOS_POWERUP	EQU	445BH	;Select and power up the specified drive
+DOS_TEST_MOUNT	EQU	445EH	;Test for mounted diskette
+DOS_NAME_ENQ	EQU	4461H	;*name routine enqueue
+DOS_NAME_DEQ	EQU	4464H	;*name routine dequeue
+;
+MESS_DO		EQU	4467H
+MESS_PR		EQU	446AH
+;
+DOS_TIME	EQU	446DH	;convert clock time to HH:MM:SS format
+DOS_DATE	EQU	4470H	;convert date to MM/DD/YY format
 DOS_EXTEND	EQU	4473H	;add deflt extnsn
+;
 $GET		EQU	0013H	;read byte from dev
+DOS_READ_BYTE	EQU	0013H	;read byte from file
 $PUT		EQU	001BH	;write byte
-DOS_ENQUEUE	EQU	4410H	;enqueue INT rtn
+DOS_WRIT_BYTE	EQU	001BH	;write byte to file
 ;
 ; Dos Errors.
 DE_DEV_NA	EQU	08H
@@ -49,18 +69,15 @@ DE_EXISTS	EQU	35H
 ;
 ; Common addresses.
 	IF	MODEL1
+DCB$VDU		EQU	401DH
 TICKER		EQU	4040H	;Interrupt tick..
 HIMEM		EQU	4049H
 COM_BUFF	EQU	4318H
-DCB$VDU		EQU	401DH
 	ENDIF
 ;
 	IF	MODEL3
 HIMEM		EQU	4411H
 COM_BUFF	EQU	4422H
 	ENDIF
-;
-MESS_DO		EQU	4467H
-MESS_PR		EQU	446AH
 ;
 *LIST ON
