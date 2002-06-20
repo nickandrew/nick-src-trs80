2f49a84bfd4fbb6e19afd663eafdf6748bf71929
Nick Andrew <nick@nick-andrew.net>
2002-06-20 22:23:50 +1000

    Updated 1987-09-12

---
+++ zeta-bbs/include/fidonet.asm
@@ -0,0 +1,19 @@
+;fidonet: Some constants & other useful stuff for Fido
+;Last updated: 12-Sep-87
+;
+ZETA_NODE	EQU	602	;was 602
+ZETA_NET	EQU	713	;was 712
+;
+ZETA_NUM	MACRO
+		DEFM	'713/602'
+		ENDM
+;
+SCAN_NUM	MACRO
+		DEFM	'713/606'
+		ENDM
+;
+ZETA_NAME	MACRO
+		DEFM	'Zeta'
+		ENDM
+;
+;This file used by PKTDIS, PKTASS, ???
