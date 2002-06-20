7439755cd1146a7398370ba3400657e9f4594526
Nick Andrew <nick@nick-andrew.net>
2002-06-20 22:36:53 +1000

    Updated 1987-08-25

--- include/include/libc.asm
+++ include/include/libc.asm
@@ -1,7 +1,6 @@
-;libc/asm: Standard I-O library for Small-C, Trs-80
+;libc.asm: Standard I-O library for Small-C, Trs-80
 ;
-	COM	'<libc dated 22 Mar 87>'
-;
+	COM	'<libc dated 25-Aug-87>'
 ;
 EOF	EQU	-1
 NULL	EQU	0
@@ -16,7 +15,7 @@ KBD_DCB		EQU	4015H
 VDU_DCB		EQU	401DH
 PTR_DCB		EQU	4025H
 ;
-FD_ARRAY	DEFS	MAX_FILES*FD_LEN
+FD_ARRAY	DC	MAX_FILES*FD_LEN,0
 ;
 ;Contents:
 FD_FLAG		EQU	0
@@ -24,6 +23,9 @@ FD_FCBPTR	EQU	1
 ;
 IS_USED	EQU	0	;bits of fd_flag
 IS_TERM	EQU	1	;its a terminal or dcb.
+IS_CHAR	EQU	1	;Is a character device
+ECHO	EQU	2
+CBREAK	EQU	3
 ;
 STDIN	EQU	0*FD_LEN+FD_ARRAY
 STDOUT	EQU	1*FD_LEN+FD_ARRAY
@@ -37,6 +39,7 @@ BUFSIZ	EQU	256
 ;int fileno(fp);
 ;FILE *fp;
 ;
+	IFREF	_FILENO
 _FILENO	LD	HL,2
 	ADD	HL,SP
 	LD	E,(HL)
@@ -55,17 +58,37 @@ $C_01	INC	BC
 $C_02	PUSH	BC
 	POP	HL
 	RET
+	ENDIF	;ifref _fileno
 ;
-;int fputc(c,fp)
-;int c;
-;FILE *fp;
-;
-_FPUTC
+;putchar(c)
+;int  c;
+	IFREF	_PUTCHAR
+_PUTCHAR
 	LD	HL,2
 	ADD	HL,SP
 	LD	E,(HL)
 	INC	HL
 	LD	D,(HL)
+	PUSH	DE
+	LD	HL,STDOUT
+	PUSH	HL
+	CALL	_FPUTC
+	POP	BC
+	POP	BC
+	RET
+	ENDIF
+;
+;int fputc(c,fp)
+;int c;
+;FILE *fp;
+;
+	IFREF	_FPUTC
+_FPUTC
+	LD	HL,2
+	ADD	HL,SP
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)	;de=fp
 	INC	HL
 	LD	C,(HL)
 	EX	DE,HL	;hl now points to fdarray
@@ -82,10 +105,183 @@ _FPUTC
 	RET
 $C_03	LD	HL,EOF
 	RET
+	ENDIF	;ifref _fputc
+;
+	IFREF	_GETCHAR
+_GETCHAR
+	LD	HL,STDIN
+	PUSH	HL
+	CALL	_FGETC
+	POP	BC
+	RET
+	ENDIF
+;
+;int getc(fp)
+;FILE *fp;
+;
+	IFREF	_GETC
+_GETC
+	JP	_FGETC
+	ENDIF
+;
+;fgets(s,n,ioptr)
+;char *s;
+;int  n;
+;FILE *ioptr;
+	IFREF	_FGETS
+_FGETS
+	LD	HL,2
+	ADD	HL,SP
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)		;DE = ioptr
+	LD	(FG_IO),DE
+	INC	HL
+	LD	C,(HL)
+	INC	HL
+	LD	B,(HL)		;BC = n
+	LD	(FG_N),BC
+	INC	HL
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)
+	EX	DE,HL		;HL = s
+	LD	(FG_S),HL
+	LD	A,B
+	OR	C
+	JP	Z,FG_8		;if count = 0
+FG_1
+	DEC	BC
+	LD	A,B
+	OR	C
+	JP	Z,FG_8
+	PUSH	HL
+	PUSH	BC
+	LD	DE,(FG_IO)
+	PUSH	DE
+	CALL	_FGETC
+	POP	IY
+	POP	BC
+	EX	DE,HL		;de = char or EOF
+	LD	HL,EOF
+	OR	A
+	SBC	HL,DE
+	JR	NZ,FG_2
+;Eof seen....
+	POP	HL		;string *
+FG_8	LD	(HL),0
+	LD	DE,(FG_S)
+	OR	A
+	SBC	HL,DE		;if hl = 0 then return HL
+	RET	Z
+	EX	DE,HL		;else return S (de)
+	RET
+;
+FG_2
+	POP	HL		;hl = string *
+	LD	(HL),E
+	INC	HL
+	LD	A,E
+	CP	0DH		;NL,CR, whatever
+	JR	NZ,FG_1
+	JR	FG_8
+;
+FG_N	DEFW	0
+FG_IO	DEFW	0
+FG_S	DEFW	0
+;
+	ENDIF	;_fgets
+;
+;fread(ptr,sizeof ptr, nitems, ioptr)
+;char *ptr;
+;int  nitems;
+;FILE *ioptr;
+	IFREF	_FREAD
+_FREAD
+	LD	HL,2
+	ADD	HL,SP
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)
+	INC	HL
+	LD	(FR_IO),DE
+	LD	C,(HL)
+	INC	HL
+	LD	B,(HL)
+	INC	HL
+	LD	(FR_NI),BC
+	LD	C,(HL)
+	INC	HL
+	LD	B,(HL)
+	INC	HL
+	LD	(FR_SI),BC
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)
+	LD	(FR_PTR),DE
+	LD	HL,0
+	LD	(FR_NREAD),HL
+	EX	DE,HL
+	LD	BC,(FR_NI)
+FR_01
+	LD	A,B
+	OR	C
+	JR	Z,FR_99		;Read NITEMS items
+	PUSH	BC
+;
+	LD	BC,(FR_SI)
+FR_02
+	PUSH	BC
+	PUSH	HL
+;
+	LD	HL,(FR_IO)
+	PUSH	HL
+	CALL	_FGETC
+	POP	IY
+;
+	LD	A,H
+	OR	A
+	JR	NZ,FR_98	;Hit EOF
+	LD	A,L
+;
+	LD	HL,(FR_NREAD)
+	INC	HL
+	LD	(FR_NREAD),HL
+;
+	POP	HL
+	LD	(HL),A
+	INC	HL
+;
+	POP	BC
+	DEC	BC
+	LD	A,B
+	OR	C
+	JR	NZ,FR_02
+;
+	POP	BC
+	DEC	BC
+	JR	FR_01
+;
+FR_98
+	POP	IY
+	POP	IY
+	POP	IY
+FR_99
+	LD	HL,(FR_NREAD)
+	RET
+;
+FR_NI	DEFW	0
+FR_IO	DEFW	0
+FR_SI	DEFW	0
+FR_PTR	DEFW	0
+FR_NREAD DEFW	0
+;
+	ENDIF	;_fread
 ;
 ;int fgetc(fp)
 ;FILE *fp;
 ;
+	IFREF	_FGETC
 _FGETC
 	LD	HL,2
 	ADD	HL,SP
@@ -93,6 +289,8 @@ _FGETC
 	INC	HL
 	LD	D,(HL)
 	EX	DE,HL
+	BIT	IS_TERM,(HL)
+	JR	NZ,FG_01
 	LD	DE,FD_FCBPTR
 	ADD	HL,DE
 	LD	E,(HL)
@@ -104,11 +302,28 @@ _FGETC
 	RET	Z
 	LD	HL,EOF
 	RET
+FG_01				;fgetc from terminal
+	LD	DE,FD_FCBPTR
+	ADD	HL,DE
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)
+FG_02	CALL	$GET
+	OR	A
+	JR	Z,FG_02
+	LD	L,A
+	LD	H,0
+	CP	04H		;eof char
+	RET	NZ
+	LD	HL,EOF
+	RET
+	ENDIF
 ;
 ;fputs(s,fp)
 ;char *s;
 ;FILE *fp;
 ;
+	IFREF	_FPUTS
 _FPUTS
 	LD	HL,2
 	ADD	HL,SP
@@ -133,11 +348,14 @@ $C_04	LD	A,(HL)
 	RET	NZ
 	INC	HL
 	JR	$C_04
+	ENDIF
 ;
 ;exit(errcode)
 ;char errcode;
 ;
+	IFREF	_EXIT
 _EXIT
+	CALL	CLOSEALL
 	LD	HL,2
 	ADD	HL,SP
 	LD	A,(HL)
@@ -145,9 +363,27 @@ _EXIT
 	JP	Z,DOS
 	JP	DOS_DISP_ERROR
 ;
+CLOSEALL			;Close all open files
+	LD	B,MAX_FILES
+	LD	HL,FD_ARRAY
+CL_1	PUSH	BC
+	PUSH	HL
+	PUSH	HL
+	CALL	_FCLOSE
+	POP	IY
+	POP	HL
+	LD	DE,FD_LEN
+	ADD	HL,DE
+	POP	BC
+	DJNZ	CL_1
+	RET
+;
+	ENDIF
+;
 ;strcpy(out,in)
 ;char *out,*in;
 ;
+	IFREF	_STRCPY
 _STRCPY
 	LD	HL,2
 	ADD	HL,SP
@@ -165,9 +401,11 @@ $C_05	LD	A,(BC)
 	OR	A
 	JR	NZ,$C_05
 	RET
+	ENDIF
 ;
 ;fclose(fp)
 ;FILE *fp;
+	IFREF	_FCLOSE
 _FCLOSE
 	LD	HL,2
 	ADD	HL,SP
@@ -175,6 +413,8 @@ _FCLOSE
 	INC	HL
 	LD	D,(HL)
 	EX	DE,HL
+	BIT	IS_USED,(HL)
+	RET	Z
 	BIT	IS_TERM,(HL)
 	LD	(HL),0
 	RET	NZ
@@ -185,24 +425,12 @@ _FCLOSE
 	LD	D,(HL)
 	CALL	DOS_CLOSE
 	RET
-;
-;int getc(fp)
-;FILE *fp;
-;
-_GETC
-	JP	_FGETC
-;
-;fprintf(fp,format,args)
-;FILE *fp;
-;char *format;
-;???? args;
-;
-_FPRINTF
-	RET
+	ENDIF
 ;
 ;FILE *fopen(file,mode)
 ;char *file,*mode;
 ;
+	IFREF	_FOPEN
 _FOPEN
 	XOR	A
 	LD	($C_07V),A	;Device name or 0.
@@ -244,17 +472,17 @@ _FOPEN
 $C_07	LD	A,(HL)
 	OR	A
 	JR	Z,$C_09
-	CP	'.'
-	JR	Z,$C_08
+;;	CP	'/'
+;;	JR	Z,$C_08
 	LD	(DE),A
 	INC	HL
 	INC	DE
 	JR	$C_07
-$C_08	LD	A,'/'
-	LD	(DE),A
-	INC	HL
-	INC	DE
-	JR	$C_07
+;;$C_08	LD	A,'.'
+;;	LD	(DE),A
+;;	INC	HL
+;;	INC	DE
+;;	JR	$C_07
 $C_09	LD	A,3
 	LD	(DE),A
 ;
@@ -267,16 +495,24 @@ $C_09	LD	A,3
 	JR	Z,$C_10
 	CP	'w'
 	JR	Z,$C_10
+;
+;Open for reading.
 	CALL	DOS_OPEN_EX	;assume read
-	LD	HL,EOF
+	LD	HL,NULL
 	RET	NZ
 	LD	HL,256+32
 	ADD	HL,DE
 	LD	(HIGHEST),HL
+;Set fcb such that writes do not truncate file
+	INC	DE
+	LD	A,(DE)
+	SET	6,A
+	LD	(DE),A
+	DEC	DE
 	JR	$C_11
 $C_10
 	CALL	DOS_OPEN_NEW	;Write & append
-	LD	HL,EOF
+	LD	HL,NULL
 	RET	NZ
 	LD	HL,256+32
 	ADD	HL,DE
@@ -296,7 +532,7 @@ $C_12
 	JR	Z,$C_14
 	ADD	HL,DE
 	DJNZ	$C_12
-$C_13	LD	HL,EOF
+$C_13	LD	HL,NULL
 	RET
 $C_14
 	LD	(HL),1
@@ -319,39 +555,157 @@ $C_07V	DEFB	0	;Type of special device C,D,L
 $C_08V	DEFW	0	;Addr of dcb/fcb.
 ;
 NULL_DCB	DC	8,0
+	ENDIF
 ;
 ;int feof(fp)
 ;FILE *fp;
+	IFREF	_FEOF
 _FEOF
 	LD	HL,2
 	ADD	HL,SP
 	LD	E,(HL)
 	INC	HL
+	LD	D,(HL)	;DE points to fdarray
+	BIT	IS_TERM,(HL)
+	JR	NZ,$C_17
+	LD	HL,FD_FCBPTR
+	ADD	HL,DE
+	LD	E,(HL)
+	INC	HL
 	LD	D,(HL)
 	PUSH	DE
 	POP	IX
-	LD	A,(IX+11)
-	CP	(IX+13)
+	LD	A,(IX+11)	;Next high
+	CP	(IX+13)		;EOF  high
 	JR	NZ,$C_15
-	LD	A,(IX+10)
-	CP	(IX+12)
+	LD	A,(IX+10)	;Next mid
+	CP	(IX+12)		;EOF  mid
 	JR	NZ,$C_15
-	LD	A,(IX+8)
-	CP	(IX+5)
+	LD	A,(IX+5)	;Next low
+	CP	(IX+8)		;EOF  low
 	JR	NZ,$C_15
-	LD	HL,0
+	LD	HL,1
 	RET
 $C_15	LD	HL,1
+	RET	NC
+$C_17	LD	HL,0
 	RET
+	ENDIF
 ;
 ;fflush(fp)
 ;FILE *fp
+	IFREF	_FFLUSH
 _FFLUSH
 	RET
+	ENDIF
+;
+;fseek(fp,offset,n)
+;FILE *fp;
+;int offset;   /* should be long */
+;int n;
+	IFREF	_FSEEK
+_FSEEK
+	LD	HL,2
+	ADD	HL,SP
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)		;de = N
+	INC	HL
+	LD	C,(HL)
+	INC	HL
+	LD	B,(HL)		;bc = offset
+	INC	HL
+	PUSH	BC
+	LD	C,(HL)
+	INC	HL
+	LD	B,(HL)		;bc = ioptr
+	LD	HL,FD_FCBPTR
+	ADD	HL,BC
+	LD	C,(HL)
+	INC	HL
+	LD	B,(HL)		;bc = fcb
+	PUSH	BC
+	POP	IX
+	LD	A,E
+	OR	A
+	JR	Z,FSE_0
+	DEC	A
+	JR	Z,FSE_1
+	LD	C,(IX+13)	;eof high
+	LD	H,(IX+12)	;eof mid
+	LD	L,(IX+8)	;eof low
+	JR	FSE_N
+FSE_1
+	LD	C,(IX+11)
+	LD	H,(IX+10)
+	LD	L,(IX+5)
+	JR	FSE_N
+FSE_0
+	LD	C,0
+	LD	H,C
+	LD	L,C
+FSE_N
+	POP	DE
+	LD	B,0
+	BIT	7,D	;check if signed
+	JR	Z,FSE_N1
+	LD	B,0FFH
+FSE_N1
+	ADD	HL,DE
+	LD	A,B
+	ADC	A,C
+	LD	C,L
+	LD	L,H
+	LD	H,A
+	PUSH	IX
+	POP	DE
+	CALL	DOS_POS_RBA
+	RET
+	ENDIF
+;
+;rewind(ioptr)
+;char *ioptr;
+	IFREF	_REWIND
+_REWIND
+	LD	HL,2
+	ADD	HL,SP
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)
+	LD	HL,FD_FCBPTR
+	ADD	HL,DE
+	LD	E,(HL)
+	INC	HL
+	LD	D,(HL)
+	CALL	DOS_REWIND
+	RET
+	ENDIF	;_rewind
 ;
 ;sprintf(string,format,args)
 ;char *string,*format;
 ;unkn args;
+	IFREF	_SPRINTF
 _SPRINTF
 	RET
+	ENDIF
 ;
+;fprintf(fp,format,args)
+;FILE *fp;
+;char *format;
+;???? args;
+;
+	IFREF	_FPRINTF
+_FPRINTF
+	RET
+	ENDIF
+;
+;printf(format,args)
+;char *format;
+;???? args;
+;
+	IFREF	_PRINTF
+_PRINTF
+	RET
+	ENDIF
+;
+;-----------------------------
