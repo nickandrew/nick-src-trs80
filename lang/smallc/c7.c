3e8b8dce4b815e0d8ef876ccd18f317f8adfa147
Nick Andrew <nick@nick-andrew.net>
2021-01-18 04:20:37 +1100

    Fix missing function args

--- lang/smallc/c7.c
+++ lang/smallc/c7.c
@@ -169,8 +169,8 @@ int label, parens;
     for (;;) {
         setstage(&before, &start);
 
-        if (heir1(lval, &lvsym, &lvopfp))
-            rvalue(lval, &lvsym, &lvopfp);
+        if (heir1(lval, &lvsym, &lvopfp, &lvstgp))
+            rvalue(lval, &lvsym, &lvopfp, &lvstgp);
 
         if (match(","))
             clearstage(before, start);
