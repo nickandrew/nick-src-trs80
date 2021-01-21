a14cb17e20073c1d8e96281332de9aba3adf0276
Nick Andrew <nick@nick-andrew.net>
2021-01-21 14:07:01 +1100

    Move *.h include files for smallc into include/smallc

---
+++ include/smallc/stdio.h
@@ -0,0 +1,10 @@
+/* standard IO file for Small-C Trs-80 */
+/*  STDIO.H */
+
+#define EOF       (-1)
+#define BUFSIZ    256
+#define NULL      0
+#define FILE      char
+#define SEP       '.'
+
+extern  char      *malloc();
