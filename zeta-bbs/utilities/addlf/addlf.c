1912566ee76cfbd3a8c741fcc60a48f0c6ab699e
Nick Andrew <nick@nick-andrew.net>
2021-01-21 17:30:52 +1100

    Fix obvious errors

--- zeta-bbs/utilities/addlf/addlf.c
+++ zeta-bbs/utilities/addlf/addlf.c
@@ -1,39 +1,42 @@
-/* addlf/ccc: add linefeeds after CR. */
+/* addlf.c: add linefeeds after CR. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 main(argc, argv)
 int argc;
 char **argv;
 {
-    int fi;
+    FILE *fi, *fo;
     char c;
+
     if (argc != 3) {
         printf("Usage: ADDLF filein fileout\n");
-        exit();
+        exit(4);
     }
+
     if ((fi = fopen(argv[1], "r")) == NULL) {
         printf("Can't open %s\n", argv[1]);
-        exit();
-    } else {
-        if ((fo = fopen(argv[2], "w")) == NULL) {
-            printf("Can't open %s\n", argv[2]);
-            exit();
-        }
+        exit(4);
     }
+
+    if ((fo = fopen(argv[2], "w")) == NULL) {
+        printf("Can't open %s\n", argv[2]);
+        exit(4);
+    }
+
     while ((c = getc(fi)) != EOF) {
         if (c == '\n')
-            add();
+            putc(0x0d, fo);
+        c = getc(fi);
+        if (c == EOF || c == 0x0a) {
+            c = 0x0a;
+        } else
+            putc(0x0a, fo);
         putc(c, fo);
     }
+
     fclose(fi);
     fclose(fo);
-    exit();
-}
-
- /**/ add()
-{
-    putc(0x0d, fo);
-    c = getc(fi);
-    if (c == EOF || c == 0x0a) {
-        c = 0x0a;
-    } else
-        putc(0x0a, fo);
+    exit(0);
 }
