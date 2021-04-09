372d41ac8ae8273e8b8a5684b96a1e4df644615c
Nick Andrew <nick@nick-andrew.net>
2021-04-09 12:34:04 +0000

    Make gettok.c compile with sdcc

--- utilities/gettok/gettok.c
+++ utilities/gettok/gettok.c
@@ -2,16 +2,21 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
 
-main()
+int main()
 {
     int toknum, labelnum, i;
     FILE *fp;
     char *locn, name[10];
 
-    if ((fp = fopen("tokens/asm", "w")) == NULL)
-        exit(0);
+    if ((fp = fopen("tokens.asm", "w")) == NULL) {
+        return 1;
+    }
+
+    /* Address in ROM of the start of the BASIC token table. Model 1 only? */
     locn = (char *) 0x1650;
+
     for (labelnum = 1, toknum = 128; toknum < 251; toknum++, labelnum++) {
         i = 1;
         *name = (*(locn++) & 0x7f);
@@ -22,5 +27,7 @@ main()
         fprintf(fp, "TOK_%d\tDEFM\t'%s',0\n", labelnum, name);
         printf("%5d %s\n", labelnum, name);
     }
+
     fclose(fp);
+    return 0;
 }
