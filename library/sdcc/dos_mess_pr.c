450af1648b5c0f1f668fa881a827dbc1c2be7886
Nick Andrew <nick@nick-andrew.net>
2021-04-13 08:55:35 +0000

    Print a message to display/printer

---
+++ library/sdcc/dos_mess_pr.c
@@ -0,0 +1,18 @@
+#include "doscalls.h"
+
+void dos_mess_pr(const char *s) __naked {
+  s;    // Avoid unreferenced function argument warning
+
+  __asm
+
+  ld iy,#2
+  add iy,sp
+  ld l,0(iy)  ; s low
+  ld h,1(iy)  ; s high
+  push ix
+  call 0x446a    ; MESS_PR
+  pop ix
+  ret
+
+  __endasm;
+}
