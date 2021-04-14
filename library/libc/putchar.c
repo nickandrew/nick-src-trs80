ab66f71bbb6f100d33dac55f1b2fa421cf2b3cc3
Nick Andrew <nick@nick-andrew.net>
2021-04-14 08:08:29 +0000

    Add temporary putchar() implementation

---
+++ library/libc/putchar.c
@@ -0,0 +1,29 @@
+/*  putchar.c - Temporary implementation of putchar()
+**
+**  putchar(c) is equivalent to putc(c, stdout), however until stdout
+**  exists, and it's possible to redirect it, implement putchar()
+**  to display directly to the screen.
+*/
+
+#include <stdio.h>
+
+int putchar(int c) __naked
+{
+  c;
+
+  __asm
+
+  ld   iy, #2   ; Skip over return address
+  add  iy,sp
+  ld   a,0(iy)  ; c low
+  cp   #0x0a    ; change 0x0a to 0x0d
+  jr   nz,001$
+  ld   a,#0x0d
+001$:
+  ld   de,#0x401d ; VDU_DCB
+  call 0x001B
+  ld   hl,#0
+  ret
+
+  __endasm;
+}
