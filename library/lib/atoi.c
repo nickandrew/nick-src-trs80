c6efc1f27e4dc29dfce98338c3e0618f9ad6d41b
Nick Andrew <nick@nick-andrew.net>
2022-11-29 08:19:05 +0000

    Make atoi.c compile under sdcc

--- library/lib/atoi.c
+++ library/lib/atoi.c
@@ -2,23 +2,26 @@
 ** atoi.c: Hacked from Alcor's CLIB/C
 */
 
+#include <string.h>
 
-_atoi(ptr)                      /* decode an integer */
-char **ptr;                     /* pointer to addr of 1st digit */
+extern int isdigit(char);
+extern int isspace(char);
+
+/* decode an integer */
+int _atoi(char **ptr)               /* pointer to addr of 1st digit */
 {
     int n;
-    int isdigit();
     n = 0;
 
-    while (isdigit(*(*ptr)))
+    while (isdigit(**ptr))
         n = 10 * n + *(*ptr)++ - '0';
     return n;
 }
 
-_itoa(n, bufptr, base)          /* integer encode routine */
-int n;                          /* unsigned integer */
-char **bufptr;                  /* pointer to the buffer pointer */
-int base;                       /* number base */
+void _itoa(unsigned int n, char **bufptr, int base)          /* integer encode routine */
+/* int n;                          unsigned integer */
+/* char **bufptr;                  pointer to the buffer pointer */
+/* int base;                       number base */
 {
     if (n < base) {
         if (n < 10)
@@ -31,37 +34,39 @@ int base;                       /* number base */
     _itoa(n % base, bufptr, base);
 }
 
-atoi(s)                         /* decode an integer */
-char *s;                        /* pointer to integer string */
+int atoi(const char *s)                         /* decode an integer */
 {
     int n;
     int sflag;
-    int isspace();
-    int _atoi();
 
     n = 0;
     sflag = 0;
     while (isspace(*s))
         ++s;
     if (*s == '+' || *s == '-')
-        if (*s++ == '-')
+        if (*(s++) == '-')
             ++sflag;
-    return _atoi(&s);
+    if (sflag) {
+      return -_atoi(&s);
+    } else {
+      return _atoi(&s);
+    }
 }
 
-itoa(n, s)
-int n;
-char *s;
+char *itoa(int n, char *s)
 {
-    int _itoa();
+    char *buf;
+
+    buf = s;
     if (n < 0) {
         if (n == -32768) {
             strcpy(s, "-32768");
-            return;
+            return s;
         }
-        *s++ = '-';
+        *buf++ = '-';
         n = -n;
     }
-    _itoa(n, &s, 10);
-    *s = '\0';
+    _itoa(n, &buf, 10);
+    *buf = '\0';
+    return s;
 }
