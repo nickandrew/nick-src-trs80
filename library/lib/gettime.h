18ed559be823bebe2cc51b01ab40f15b7b76bbfc
Nick Andrew <nick@nick-andrew.net>
2022-12-04 08:01:11 +0000

    Add gettime.h

---
+++ library/lib/gettime.h
@@ -0,0 +1,8 @@
+/* gettime.h */
+
+extern unsigned char getsecon(void);
+extern unsigned char getminut(void);
+extern unsigned char gethour(void);
+extern unsigned char getday(void);
+extern unsigned char getmonth(void);
+extern unsigned char getyear(void);
