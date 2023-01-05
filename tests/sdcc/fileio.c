e2f6bfa179905bea0ffcbf7e52646412256f47af
Nick Andrew <nick@nick-andrew.net>
2023-01-05 14:33:04 +1100

    Add tests for fgetc(), ftell(), fputc(), fputs()

--- tests/sdcc/fileio.c
+++ tests/sdcc/fileio.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
+void test_read(void) {
+  FILE *fp = fopen("testhw/src", "r");
+  printf("fopen(testhw/src, r) returned %x\n", (int) fp);
+  int i;
+
+  for (i = 0; i < 10; ++i) {
+    int rc = fgetc(fp);
+    if (rc < 0) {
+      printf("fgetc() returned %d - eof\n", rc);
+      break;
+    }
+    long fpos = ftell(fp);
+    printf("fgetc() returned %04x at pos %ld\n", rc, fpos);
+  }
+  fclose(fp);
+}
+
+void test_write(void) {
+  FILE *fp = fopen("output:1", "r");
+  int rc;
+  rc = fputc('\n', fp);
+  printf("fputc() rc %d\n", rc);
+  rc = putc('\n', fp);
+  printf("putc() rc %d\n", rc);
+  rc = fputs("DATA\n", fp);
+  printf("fputs() rc %d\n", rc);
+  long fpos = ftell(fp);
+  printf("ftell() rc %ld\n", fpos);
+  fclose(fp);
+}
+
 int main()
 {
-  FILE *fp = fopen("testhw/src", "r");
-  printf("fopen() returned %x\n", (int) fp);
+  test_read();
+  test_write();
   return 0;
 }
