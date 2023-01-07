fccf6dc47095d18d2c9c14cb10a45ee74f87bd5b
Nick Andrew <nick@nick-andrew.net>
2023-01-07 05:03:49 +0000

    Always search $LIB_SDCC in include.mk

--- library/sdcc/include.mk
+++ library/sdcc/include.mk
@@ -12,7 +12,7 @@ CRT0=$(LIB_SDCC)/crt0.rel
 	sdcc $(INCLUDES) -mz80 -c $<
 
 %.ihx %.lk: %.rel
-	sdcc -mz80 --code-loc 0x5200 --data-loc 0x0000 -o $@ $(Z80LIBDIR) $(EXTRALIBDIR) --no-std-crt0 $(CRT0) $+ $(EXTRALIBS)
+	sdcc -mz80 --code-loc 0x5200 --data-loc 0x0000 -o $@ $(Z80LIBDIR) -L $(LIB_SDCC) $(EXTRALIBDIR) --no-std-crt0 $(CRT0) $+ $(EXTRALIBS)
 
 # Edit the Intel Hex file to hardcode the program entry address to 0x5200
 %.cmd: %.ihx
