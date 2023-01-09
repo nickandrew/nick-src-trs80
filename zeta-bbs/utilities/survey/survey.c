4af8818bf7db87aac30527d843247a704bb94173
Nick Andrew <nick@nick-andrew.net>
2023-01-09 20:34:19 +1100

    Hack zeta-bbs/utilities/survey to work

--- zeta-bbs/utilities/survey/survey.c
+++ zeta-bbs/utilities/survey/survey.c
@@ -1,5 +1,5 @@
 /* Zetasource
-**  survey1.c ... Ask the non-member some survey questions
+**  survey.c ... Ask the non-member some survey questions
 **  Ver 1.0a on 23-Jan-88
 */
 
@@ -7,33 +7,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define SURVEY   "quest.zms"
-#define RESULT   "answers.zms"
+#ifdef __SDCC_z80
+#include <rom.h>
+#include <zeta.h>
+#endif
 
-void std_out(char *s);
 void init(void);
-void signon(void);
 int question(void);
 int readq(void);
 
-// Somewhere else
-#include <string.h>
-void getuname(char *line) { strcpy(line, "Big Dummy"); }
+const char *in_file;
+const char *out_file;
 
-FILE *in, *out;
-char line[80], qname[20];
-char prompt[] = "\n>  ";
-
-int main(void)
-{
-    init();
-    signon();
-    while (question()) ;
-    fputs("\n", out);
-    fclose(out);
-    std_out("\n\nThanks for taking the time to answer this survey.\n");
-    return 0;
-}
+FILE *in_fp, *out_fp;
+char line[80];    // Question text read from survey file, and response text
+char qname[20];   // Question name ("qname>text" in survey file)
+char prompt[] = "> ";
 
 void std_out(char *s)
 {
@@ -42,42 +31,24 @@ void std_out(char *s)
 
 void init(void)
 {
-    if ((in = fopen(SURVEY, "r")) == NULL) {
+    if ((in_fp = fopen(in_file, "r")) == NULL) {
         std_out("Couldn't open survey file, sorry!\n");
         exit(1);
     }
 
-    if ((out = fopen(RESULT, "a")) == NULL) {
+    if ((out_fp = fopen(out_file, "a")) == NULL) {
         std_out("Couldn't open results file, sorry!\n");
         exit(1);
     }
 
-    fputs("-----\n", out);
+    fputs("-----\n", out_fp);
+    // Zeta function to return the current user name
     getuname(line);
-    fputs(line, out);
-    fputs("\n", out);
+    fputs(line, out_fp);
+    fputs("\n", out_fp);
 }
 
-void signon(void)
-{
-    readq();
-    std_out("\n\n");
-}
-
-int question(void)
-{
-    std_out("\n");
-    if (readq())
-        return 0;
-    std_out(prompt);
-    fgets(line, sizeof(line), stdin);
-    fputs(qname, out);
-    fputs(": ", out);
-    fputs(line, out);
-    return 1;
-}
-
-// Read a question from 'in' and display on the screen.
+// Read a question from 'in_fp' and display on the screen.
 // Questions are one of the forms:
 //     Question Text
 //     question name>Question Text
@@ -90,7 +61,7 @@ int readq(void)
     strcpy(qname, "Q");
 
     while (1) {
-        if (fgets(line, 80, in) == NULL) {
+        if (fgets(line, sizeof(line), in_fp) == NULL) {
             return 1;
         }
         cp = line;
@@ -113,4 +84,72 @@ int readq(void)
         }
         std_out(cp);
     }
+}
+
+// Return -1 if question not answered
+// (BREAK or EOF seen)
+
+int ask_question(void)
+{
+#ifdef __SDCC_z80
+  // Read up to 78 text characters. Append \n then \0
+  int rc = rom_kbline(line, sizeof(line) - 2);
+  if (rc < 0) {
+    return -1;
+  }
+  strcat(line, "\n");
+  return 0;
+#else
+  if (fgets(line, sizeof(line), stdin) == NULL) {
+    return -1;
+  }
+  return 0;
+#endif
+}
+
+// Ask the next question, prompt for an answer; write the answer to the
+// output file.
+
+int question(void)
+{
+    std_out("\n");
+    if (readq())
+        return 0;
+    std_out(prompt);
+    int aq = ask_question();
+    if (aq < 0) {
+      return 0;
+    }
+
+    fputs(qname, out_fp);
+    fputs(": ", out_fp);
+    fputs(line, out_fp);
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 3) {
+      in_file = argv[1];
+      out_file = argv[2];
+    } else {
+      in_file = "survey/zms";
+      out_file = "answers/zms";
+    }
+
+    init();
+
+    // Read and print the introduction
+    readq();
+    std_out("\n\n");
+
+    while (question()) ;
+
+    fclose(in_fp);
+
+    fputs("\n", out_fp);
+    fclose(out_fp);
+
+    std_out("\nThanks for taking the time to answer this survey.\n");
+    return 0;
 }
