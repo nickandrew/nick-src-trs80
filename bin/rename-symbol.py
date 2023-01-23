3d8c519ff20a830dc2331ece445a324153e4a5e4
Nick Andrew <nick@nick-andrew.net>
2023-01-24 03:20:36 +1100

    Rename multiple symbols in a single pass

--- bin/rename-symbol.py
+++ bin/rename-symbol.py
@@ -6,6 +6,7 @@ import argparse
 from   asm_helpers import RenameSymbol
 import asm_parser
 import os
+import re
 import sys
 
 from lark import Token,Tree
@@ -13,8 +14,9 @@ from lark.reconstruct import Reconstructor
 from lark.visitors import Discard,Transformer,Visitor
 
 
-def process_file(filename, from_symbol, to_symbol):
-    """Edit the contents of filename and change from_symbol to to_symbol."""
+
+def process_file(filename, symbol_map):
+    """Edit the contents of filename and change symbols in symbol_map."""
     parser = asm_parser.ASMParser()
 
     with open(filename, 'r') as in_f:
@@ -24,7 +26,7 @@ def process_file(filename, from_symbol, to_symbol):
             # print(f'Unable to parse {filename}, skipping')
             return
 
-    t = RenameSymbol(from_symbol=from_symbol, to_symbol=to_symbol)
+    t = RenameSymbol(symbol_map=symbol_map)
     new_tree = t.transform(tree)
     if t.modified:
         print(f'Modified {filename}')
@@ -35,17 +37,24 @@ def process_file(filename, from_symbol, to_symbol):
 
 def parse_args():
     p = argparse.ArgumentParser(description='Refactor ASM files to remove multiple definitions')
-    p.add_argument('from_symbol', help='Symbol to be renamed')
-    p.add_argument('to_symbol', help='Symbol to rename to')
-    p.add_argument('filenames', nargs='*', help='Filenames to process')
+    p.add_argument('--map', nargs='*', help='One or more old_symbol=new_symbol')
+    p.add_argument('--files', nargs='*', help='Filenames to process')
     return p.parse_args()
-
 
 def main():
     args = parse_args()
 
-    for filename in args.filenames:
-        process_file(filename=filename, from_symbol=args.from_symbol, to_symbol=args.to_symbol)
+    symbol_map = {}
+    for s in args.map:
+        m = re.match(r'(.+)=(.+)', s)
+        if m:
+            symbol_map[m.group(1)] = m.group(2)
+
+    if not symbol_map:
+        raise ValueError('No symbol mappings were requested on command line')
+
+    for filename in args.files:
+        process_file(filename=filename, symbol_map=symbol_map)
 
 if __name__ == '__main__':
     main()
