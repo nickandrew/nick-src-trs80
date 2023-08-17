61c7986000eb6a388222e075b0c5a2e55a5bc302
Nick Andrew <nick@nick-andrew.net>
2023-08-18 09:48:11 +1000

    Add library/zeta

---
+++ include/sdcc/zeta.h
@@ -0,0 +1,8 @@
+/* zeta.h */
+
+#ifndef _ZETA_H_
+#define _ZETA_H_
+
+extern void getuname(char *buf);
+
+#endif /* _ZETA_H_ */
---
+++ library/zeta/getuname.c
@@ -0,0 +1,10 @@
+/* getuname.c - Return the current username */
+
+#include <string.h>
+#include <zeta.h>
+
+void getuname(char *buf)
+{
+	// TODO: Fill out with current username
+	strcpy(buf, "Dummy");
+}
