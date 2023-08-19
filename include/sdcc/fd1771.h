4461c0ee09d77fda68a402a27c9838475b54f1f0
Nick Andrew <nick@nick-andrew.net>
2023-08-19 06:10:22 +0000

    Add fd1771 to library/fd1771

---
+++ include/sdcc/fd1771.h
@@ -0,0 +1,62 @@
+/*  fd1771.h - Structures and function prototypes for the FD1771 disk controller.
+**
+*/
+
+#ifndef FD1771_H_
+#define FD1771_H_
+
+#define fd1771_step_rate_0 0x00
+#define fd1771_step_rate_1 0x01
+#define fd1771_step_rate_2 0x02
+#define fd1771_step_rate_3 0x03
+
+#define fd1771_verify 0x04
+
+#define fd1771_load_head 0x08
+
+#define fd1771_update_track 0x10
+
+#define fd1771_multiple 0x10
+
+#define fd1771_block_length_ibm 0x08
+
+#define fd1771_enable_hld_delay 0x04
+
+#define fd1771_data_mark 0x00
+#define fd1771_user_defined_1 0x01
+#define fd1771_user_defined_2 0x02
+#define fd1771_deleted_data_mark 0x03
+
+#define fd1771_synchronize 0
+#define fd1771_no_synchronize 1
+
+#define fd1771_int_nr_to_r 1
+#define fd1771_int_r_to_nr 2
+#define fd1771_int_index 4
+#define fd1771_int_immediate 8
+
+struct fd1771_id_buf {
+	char track_addr;
+	char zeros;
+	char sector_addr;
+	char sector_length;
+	char crc_1;
+	char crc_2;
+};
+
+extern void fd1771_delay(unsigned int delay) __sdcccall(1);
+extern char fd1771_get_sector(void) __sdcccall(1);
+extern char fd1771_get_status(void) __sdcccall(1);
+extern char fd1771_get_track(void) __sdcccall(1);
+extern unsigned int fd1771_read(char flags, char *buf) __sdcccall(1);
+extern char fd1771_read_address(struct fd1771_id_buf *buf) __sdcccall(1);
+extern char fd1771_restore(char flags) __sdcccall(1);
+extern char fd1771_seek(char flags, char track) __sdcccall(1);
+extern void fd1771_select(char selector) __sdcccall(1);
+extern void fd1771_set_sector(char sector) __sdcccall(1);
+extern void fd1771_set_track(char track) __sdcccall(1);
+extern char fd1771_step(char flags) __sdcccall(1);
+extern char fd1771_step_in(char flags) __sdcccall(1);
+extern char fd1771_step_out(char flags) __sdcccall(1);
+
+#endif
---
+++ library/fd1771/fd1771.c
@@ -0,0 +1,254 @@
+/* Interface to Trs-80 FD1771 Floppy Disk Controller
+**
+*/
+
+#include "fd1771.h"
+
+// Execute a tight 16-bit DJNZ loop
+void fd1771_delay(unsigned int delay) __naked __sdcccall(1)
+{
+	delay;  // Avoid unreferenced function argument warning
+
+	__asm
+	// Parameters passed in HL
+	ld b,l         ; Low order delay value
+00001$:
+	djnz 00001$
+	ld a,h
+	or a
+	ret z
+	dec h
+	jr 00001$       ; Loop around another 256x
+
+	__endasm;
+}
+
+// Read current sector register
+char fd1771_get_sector(void) __naked __sdcccall(1)
+{
+	__asm
+	ld a,(0x37ee)   ; Read the sector register
+	ret
+
+	__endasm;
+}
+
+// Read current status register
+char fd1771_get_status(void) __naked __sdcccall(1)
+{
+	__asm
+	ld a,(0x37ec)   ; Read the status register
+	ret
+
+	__endasm;
+}
+
+// Read current track register
+char fd1771_get_track(void) __naked __sdcccall(1)
+{
+	__asm
+	ld a,(0x37ed)   ; Read the track register
+	ret
+
+	__endasm;
+}
+
+// Read one or more sectors from the diskette.
+// Track and sector numbers need to be set in the corresponding registers in advance.
+// Return the first unused buffer address.
+unsigned int fd1771_read(char flags, char *buf) __naked __sdcccall(1)
+{
+	flags;  // Avoid unreferenced function argument warning
+	buf;  // Avoid unreferenced function argument warning
+
+	__asm
+	// flags: A, buf: HL
+	and a,#0x1f     ; Retain only flags bits.
+	or a,#0x80      ; It is now a Read command
+	ld b,a          ; Save command
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ld a,#255       ; Fake status for debugging
+	ret nz          ; Return early with status in A if controller is busy
+	ld d,h          ; Use DE as the write buffer, as it will be the return value
+	ld e,l
+	ld a,b          ; Read command
+	ld (0x37ec),a
+00001$:
+	ld a,(0x37ec)
+	bit 0,a
+	ret z           ; Controller is no longer busy; return status
+	bit 1,a
+	jr z,00001$     ; Loop until a byte is ready
+	ld a,(0x37ef)   ; Read data register
+	ld (de),a       ; Save data in buffer
+	inc de
+	jr 00001$
+
+	__endasm;
+}
+
+char fd1771_read_address(struct fd1771_id_buf *buf) __naked __sdcccall(1)
+{
+	buf;  // Avoid unreferenced function argument warning
+
+	__asm
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ld a,#255       ; Fake status for debugging
+	ret nz          ; Return early with status in A if controller is busy
+	ld a,#0xc4      ; Read Address command
+	ld (0x37ec),a
+	ld b,#6
+00001$:
+	ld a,(0x37ec)
+	bit 0,a
+	ret z           ; Controller is no longer busy; return status
+	bit 1,a
+	jr z,00001$     ; Loop until a byte is ready
+	ld a,(0x37ef)   ; Read data register
+	ld (hl),a
+	inc hl
+	djnz 00001$     ; Loop to read all 6 bytes
+00002$:
+	ld a,(0x37ec)
+	bit 0,a
+	jr nz,00002$    ; Loop until controller is no longer busy
+	ret
+
+	__endasm;
+}
+
+// Read current status byte
+char fd1771_read_status(void) __naked __sdcccall(1)
+{
+	__asm
+	ld a,(0x37ec)   ; Read the status register
+	ret
+
+	__endasm;
+}
+
+char fd1771_restore(char flags) __naked __sdcccall(1)
+{
+	flags;  // Avoid unreferenced function argument warning
+
+	__asm
+
+	and a,#0x0f     ; Retain only flags bits. It is now a restore command
+	ld b,a
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ld a,#255       ; Fake status for debugging
+	ret nz          ; Return early with status in A if controller is busy
+	ld a,b
+	ld (0x37ec),a
+00001$:
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ret z
+	jr 00001$       ; Loop until controller is non-busy
+
+	__endasm;
+}
+
+void fd1771_select(char selector) __naked __sdcccall(1)
+{
+	selector;  // Avoid unreferenced function argument warning
+
+	__asm
+	ld (0x37e1),a
+	ret
+	__endasm;
+}
+
+// Set current sector register
+void fd1771_set_sector(char sector) __naked __sdcccall(1)
+{
+	sector;  // Avoid unreferenced function argument warning
+
+	__asm
+	ld (0x37ee),a   ; Set the sector register
+	ret
+
+	__endasm;
+}
+
+// Set current track register
+void fd1771_set_track(char track) __naked __sdcccall(1)
+{
+	track;  // Avoid unreferenced function argument warning
+
+	__asm
+	ld (0x37ed),a   ; Set the track register
+	ret
+
+	__endasm;
+}
+
+// Step one track in the same direction as last step
+char fd1771_step(char flags) __naked __sdcccall(1)
+{
+	flags;  // Avoid unreferenced function argument warning
+
+	__asm
+	and a,#0x1f     ; Retain only flags bits.
+	or a,#0x20      ; It is now a Step command
+	ld b,a
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ld a,#255       ; Fake status for debugging
+	ret nz          ; Return early with status in A if controller is busy
+	ld a,b
+	ld (0x37ec),a
+00001$:
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ret z
+	jr 00001$       ; Loop until controller is non-busy
+	__endasm;
+}
+
+char fd1771_step_in(char flags) __naked __sdcccall(1)
+{
+	flags;  // Avoid unreferenced function argument warning
+
+	__asm
+	and a,#0x1f     ; Retain only flags bits.
+	or a,#0x40      ; It is now a Step In command
+	ld b,a
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ld a,#255       ; Fake status for debugging
+	ret nz          ; Return early with status in A if controller is busy
+	ld a,b
+	ld (0x37ec),a
+00001$:
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ret z
+	jr 00001$       ; Loop until controller is non-busy
+	__endasm;
+}
+
+char fd1771_step_out(char flags) __naked __sdcccall(1)
+{
+	flags;  // Avoid unreferenced function argument warning
+
+	__asm
+	and a,#0x1f     ; Retain only flags bits.
+	or a,#0x60      ; It is now a Step Out command
+	ld b,a
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ld a,#255       ; Fake status for debugging
+	ret nz          ; Return early with status in A if controller is busy
+	ld a,b
+	ld (0x37ec),a
+00001$:
+	ld a,(0x37ec)   ; Read the status register
+	bit 0,a
+	ret z
+	jr 00001$       ; Loop until controller is non-busy
+	__endasm;
+}
