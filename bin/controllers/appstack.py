0d695121b5457ec0b0bfd709f4ea400117132507
Nick Andrew <nick@nick-andrew.net>
2023-09-16 02:43:49 +0000

    autodebug: Trace push and pop operations

---
+++ bin/controllers/appstack.py
@@ -0,0 +1,25 @@
+"""Traces read_forwd_4_bytes and read_forwd_4_bytes."""
+
+class Controller(object):
+  """A Controller executes a debugging scenario - setting breakpoints, running code etc."""
+
+  def __init__(self, dbg):
+    self.dbg = dbg
+
+  def run(self):
+    """Run the debugging until the end."""
+    self.dbg.add_breakpoint('5e23', self.read_forwd)
+    self.dbg.add_breakpoint('5e2c', self.write_back)
+    self.dbg.run()
+
+  def read_forwd(self):
+    self.dbg.get_registers()
+    de = self.dbg.get_reg_de()
+    mem = self.dbg.memory(de, 4)
+    print(f'POP {de:04x} {mem}')
+
+  def write_back(self):
+    self.dbg.get_registers()
+    de = self.dbg.get_reg_de()
+    hl = self.dbg.get_reg_hl()
+    print(f'PUSH {de-4:04x} HL {hl:04x}')
