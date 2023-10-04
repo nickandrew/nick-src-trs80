b00f88c81993b8969844552921695e8d18fa1408
Nick Andrew <nick@nick-andrew.net>
2023-10-04 15:02:07 +0000

    Specify multiple memory regions to watch

--- bin/controllers/memdiff.py
+++ bin/controllers/memdiff.py
@@ -10,6 +10,28 @@ three_byte_opcodes = [0x97, 0x98, 0xa1];
 # 0xa1 ... opgoto next 2 bytes
 # 0xa2 ... looks like folloqing bytes are a jump table
 
+class MemoryRegion(object):
+  """A MemoryRegion will track changes within a continuous area of memory."""
+  def __init__(self, dbg, first_address, last_address):
+    self.dbg = dbg
+    self.saved_memory = None
+    self.first_address = first_address
+    self.last_address = last_address
+
+  def update(self):
+    print(f'---------------- region from {self.first_address:04x} to {self.last_address:04x}')
+    new_memory = self.dbg.memory(self.first_address, self.last_address - self.first_address + 1)
+
+    if self.saved_memory is not None:
+      for addr in range(0, self.last_address - self.first_address + 1):
+        om = self.saved_memory[addr]
+        nm = new_memory[addr]
+        if om != nm:
+          print(f'memory {addr + self.first_address:04x} changed from {om} to {nm}')
+
+    self.saved_memory = new_memory
+
+
 class Controller(object):
   """A Controller executes a debugging scenario - setting breakpoints, running code etc."""
 
@@ -192,9 +214,10 @@ class Controller(object):
 
   def __init__(self, dbg):
     self.dbg = dbg
-    self.saved_memory = None
     self.first_address = 0x4300
-    self.last_address = 0x437b
+    self.last_address = 0x4723
+    self.region_1 = MemoryRegion(dbg, first_address=0x4300, last_address=0x4723)
+    self.region_2 = MemoryRegion(dbg, first_address=0x4824, last_address=0x59ff)
 
   def init(self):
     """Set up debugging."""
@@ -205,16 +228,8 @@ class Controller(object):
     """Dump memory between each command."""
 
     print('----------------')
-    new_memory = self.dbg.memory(self.first_address, self.last_address - self.first_address + 1)
-
-    if self.saved_memory is not None:
-      for addr in range(0, self.last_address - self.first_address + 1):
-        om = self.saved_memory[addr]
-        nm = new_memory[addr]
-        if om != nm:
-          print(f'memory {addr + self.first_address:04x} changed from {om} to {nm}')
-
-    self.saved_memory = new_memory
+    self.region_1.update()
+    self.region_2.update()
 
   def fetch_opcode(self):
     self.dbg.get_registers()
