714875996e0274a240872478e776eae7941f1c6b
Nick Andrew <nick@nick-andrew.net>
2023-12-07 14:58:17 +0000

    tracevm: Use vm.Debug

--- bin/controllers/tracevm.py
+++ bin/controllers/tracevm.py
@@ -1,34 +1,36 @@
 """Traces virtual machine opcode fetch and execute loop."""
 
-import colossal_cave.vm
+from colossal_cave import vm
 
 class Controller(object):
   """A Controller executes a debugging scenario - setting breakpoints, running code etc."""
 
   def __init__(self, dbg):
-    self.dbg = dbg
+    self.vmdbg = vm.Debug(dbg)
 
   def init(self):
     """Set up debugging."""
-    self.dbg.add_breakpoint('5a19', self.fetch_opcode)
+    self.vmdbg.add_trace(self.trace_1)
 
-  def fetch_opcode(self):
-    self.dbg.get_registers()
-    a = self.dbg.get_reg_a()
-    bc = self.dbg.get_reg_bc()
-    # Grab the next 2 bytes of memory in case it's a 2 or 3 byte opcode
-    m_str = self.dbg.memory(bc + 1, 2)
-    memory = [a, int(m_str[0], 16), int(m_str[1], 16)]
+  def trace_1(self):
+    instruction = self.vmdbg.instruction()
+    o1 = ' '.join([f'{x:02x}' for x in instruction.memory])
+    print(f'{self.vmdbg.pc:04x}    {o1:8s} {instruction.disassemble()}')
 
-    disassembly = colossal_cave.vm.Opcodes.disassemble(bc, memory)
-    length = colossal_cave.vm.Opcodes.opcode_length(a)
+    # Additional operands
+    operand_list = []
+    if instruction.opcode.stack_count > 0:
+      for i in range(instruction.opcode.stack_count):
+        (value, pointer) = self.vmdbg.stack(i)
+        s = f'({value:04x},{pointer:04x})'
+        operand_list.append(s)
 
-    rest = ''
-    if length == 2:
-      rest = f'{m_str[0]}   '
-    elif length == 3:
-      rest = f'{m_str[0]} {m_str[1]}'
-    else:
-      rest = '     '
+    if instruction.opcode.word_address:
+      value = self.vmdbg.memory.word(instruction.opcode.word_address)
+      operand_list.append(f'mem:{value:04x}')
 
-    print(f'{bc:04x}    {a:02x} {rest} {disassembly}')
+    if operand_list:
+      operands = '          Operands: ' + ' '.join(operand_list)
+      print(operands)
+
+    return True
