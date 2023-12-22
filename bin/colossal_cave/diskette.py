7db88793b00e9d4ef5b94ada05bda5389afce0b3
Nick Andrew <nick@nick-andrew.net>
2023-12-22 02:00:39 +0000

    diskette.py: Support image creation

--- bin/colossal_cave/diskette.py
+++ bin/colossal_cave/diskette.py
@@ -1,10 +1,23 @@
 """A colossal cave diskette.
 
-The main contents of the diskette are, in order:
+The diskette is 350 x 256-byte sectors long, or 89600 bytes.
 
-  Boot sector (1 sector)
-  Game code (62 sectors)
-  Data (287 sectors)
+The image format is:
+
+  * Boot sector: 1 sector
+  * Game code: 62 sectors
+  * Data area: 287 sectors
+    * Save Game 2 data: 23 sectors
+    * Save Game 1 data: 23 sectors
+    * Gap: 1 sector
+    * Long Descriptions: 71 sectors
+    * Short Descriptions: 10 sectors
+    * Object Descriptions: 21 sectors
+    * Rtext: 80 sectors
+    * Score summaries: 4 sectors
+    * Gap: 44 sectors
+    * Backup code: 9 sectors
+    * Gap: 1 sector
 """
 
 class Diskette(object):
@@ -22,21 +35,45 @@ class Diskette(object):
     The supplied bytes buffer is split into its logical components.
     """
 
-    if buf is None:
-      buf = bytes(0x15e00)
+    self._boot_sector = None
+    self._code = None
+    self._data = None
 
-    self._boot_sector = buf[0x0000:0x0100]
-    self._code = buf[0x0100:0x3f00]
-    self._data = buf[0x3f00:]
+    if buf is not None:
+      self._boot_sector = buf[0x0000:0x0100]
+      self._code = buf[0x0100:0x3f00]
+      self._data = buf[0x3f00:]
 
-  def boot_sector(self) -> bytes:
-    """Return the boot sector code."""
+  def boot_sector(self, buf:bytes=None) -> bytes:
+    """Set/Get the boot sector code."""
+
+    if buf is not None:
+      if len(buf) != 256:
+        raise BufferError('Boot sector must be 256 bytes long')
+      self._boot_sector = buf
+
     return self._boot_sector
 
-  def code(self) -> bytes:
-    """Return the game code."""
+  def code(self, buf:bytes=None) -> bytes:
+    """Set/Get the game code."""
+
+    if buf is not None:
+      if len(buf) != 62 * 256:
+        raise BufferError('Code must be 62 * 256 bytes long')
+      self._code = buf
+
     return self._code
 
-  def data(self) -> bytes:
-    """Return the data area."""
+  def data(self, buf:bytes=None) -> bytes:
+    """Set/Get the data area."""
+
+    if buf is not None:
+      if len(buf) != 287 * 256:
+        raise BufferError('Data must be 287 * 256 bytes long')
+      self._data = buf
+
     return self._data
+
+  def image(self) -> bytes:
+    """Return the 89600 byte long game image."""
+    return self._boot_sector + self._code + self._data
