98b373e3aa5ac82a3f28c4e20f3974eddcc13e8a
Nick Andrew <nick@nick-andrew.net>
2024-01-02 03:44:45 +0000

    rename-symbol: Reindent and add a --mapfile argument

--- bin/rename-symbol.py
+++ bin/rename-symbol.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#  vim:expandtab:sw=4:ts=8:sts=4:ai:
+#  vim:expandtab:sw=2:ts=2:sts=2:ai:
 """Rename symbol A to B in multiple ASM files."""
 
 import argparse
@@ -14,47 +14,56 @@ from lark.reconstruct import Reconstructor
 from lark.visitors import Discard,Transformer,Visitor
 
 
-
 def process_file(filename, symbol_map):
-    """Edit the contents of filename and change symbols in symbol_map."""
-    parser = asm_parser.ASMParser()
+  """Edit the contents of filename and change symbols in symbol_map."""
+  parser = asm_parser.ASMParser()
 
-    with open(filename, 'r') as in_f:
-        try:
-            tree = parser.parse(in_f.read())
-        except Exception:
-            # print(f'Unable to parse {filename}, skipping')
-            return
+  with open(filename, 'r') as in_f:
+    try:
+      tree = parser.parse(in_f.read())
+    except Exception as e:
+      print(f'Unable to parse {filename} due to {e}, skipping')
+      return
 
-    t = RenameSymbol(symbol_map=symbol_map)
-    new_tree = t.transform(tree)
-    if t.modified:
-        print(f'Modified {filename}')
-        result = Reconstructor(parser.parser).reconstruct(new_tree)
-        with open(filename, 'w') as out_f:
-            print(result, end='', file=out_f)
+  t = RenameSymbol(symbol_map=symbol_map)
+  new_tree = t.transform(tree)
+  if t.modified:
+    print(f'Modified {filename}')
+    result = Reconstructor(parser.parser).reconstruct(new_tree)
+    with open(filename, 'w') as out_f:
+      print(result, end='', file=out_f)
 
 
 def parse_args():
-    p = argparse.ArgumentParser(description='Refactor ASM files to remove multiple definitions')
-    p.add_argument('--map', nargs='*', help='One or more old_symbol=new_symbol')
-    p.add_argument('--files', nargs='*', help='Filenames to process')
-    return p.parse_args()
+  p = argparse.ArgumentParser(description='Refactor ASM files to rename symbols')
+  p.add_argument('--map', nargs='*', help='One or more old_symbol=new_symbol')
+  p.add_argument('--mapfile', help='Mapping file containing old_symbol=new_symbol')
+  p.add_argument('--files', nargs='*', help='Filenames to process')
+  return p.parse_args()
+
 
 def main():
-    args = parse_args()
+  args = parse_args()
 
-    symbol_map = {}
+  symbol_map = {}
+  if args.map:
     for s in args.map:
-        m = re.match(r'(.+)=(.+)', s)
+      m = re.match(r'(.+)=(.+)', s)
+      if m:
+        symbol_map[m.group(1)] = m.group(2)
+
+  if args.mapfile:
+    with open(args.mapfile, "r") as ifp:
+      for line in ifp:
+        m = re.match(r'(.+)=(.+)\n', line)
         if m:
-            symbol_map[m.group(1)] = m.group(2)
+          symbol_map[m.group(1)] = m.group(2)
 
-    if not symbol_map:
-        raise ValueError('No symbol mappings were requested on command line')
+  if not symbol_map:
+    raise ValueError('No symbol mappings were requested on command line')
 
-    for filename in args.files:
-        process_file(filename=filename, symbol_map=symbol_map)
+  for filename in args.files:
+    process_file(filename=filename, symbol_map=symbol_map)
 
 if __name__ == '__main__':
-    main()
+  main()
